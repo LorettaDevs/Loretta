2f8343ecb646c2235bea7ffd1853c1334fa10968
GGG KILLER <gggkiller2@gmail.com>
2020-01-19 22:07:45 -0300

    Rewrite the walker base

--- Loretta/Parsing/Visitor/TreeWalkerBase.cs
+++ Loretta/Parsing/Visitor/TreeWalkerBase.cs
@@ -1,50 +1,39 @@
-﻿using System;
-using Loretta.Parsing.AST;
+﻿using Loretta.Parsing.AST;
 using Loretta.Parsing.AST.Tables;
 
 namespace Loretta.Parsing.Visitor
 {
     public abstract class TreeWalkerBase : ITreeVisitor
     {
-        public virtual void VisitNode ( LuaASTNode node ) =>
-            node?.Accept ( this );
-
-        public virtual void VisitAnonymousFunction ( AnonymousFunctionExpression node )
+        public virtual void VisitNil ( NilExpression node )
         {
-            foreach ( Expression argument in node.Arguments )
-                this.VisitNode ( argument );
-            this.VisitStatementList ( node.Body );
-        }
-
-        public virtual void VisitAssignment ( AssignmentStatement node )
-        {
-            foreach ( Expression variable in node.Variables )
-                this.VisitNode ( variable );
-            foreach ( Expression value in node.Values )
-                this.VisitNode ( value );
-        }
-
-        public virtual void VisitBinaryOperation ( BinaryOperationExpression node )
-        {
-            this.VisitNode ( node.Left );
-            this.VisitNode ( node.Right );
         }
 
         public virtual void VisitBoolean ( BooleanExpression node )
         {
         }
 
-        public virtual void VisitBreak ( BreakStatement node )
+        public virtual void VisitIdentifier ( IdentifierExpression node )
         {
         }
 
-        public virtual void VisitContinue ( ContinueStatement @continue )
+        public virtual void VisitIndex ( IndexExpression node )
+        {
+            this.VisitNode ( node.Indexee );
+            this.VisitNode ( node.Indexer );
+        }
+
+        public virtual void VisitNumber ( NumberExpression node )
         {
         }
 
-        public virtual void VisitDo ( DoStatement node ) => this.VisitStatementList ( node.Body );
+        public virtual void VisitString ( StringExpression node )
+        {
+        }
 
-        public virtual void VisitExpressionStatement ( ExpressionStatement node ) => this.VisitNode ( node.Expression );
+        public virtual void VisitVarArg ( VarArgExpression node )
+        {
+        }
 
         public virtual void VisitFunctionCall ( FunctionCallExpression node )
         {
@@ -53,11 +42,76 @@ namespace Loretta.Parsing.Visitor
                 this.VisitNode ( argument );
         }
 
+        public virtual void VisitUnaryOperation ( UnaryOperationExpression node ) =>
+            this.VisitNode ( node.Operand );
+
+        public virtual void VisitGroupedExpression ( GroupedExpression node ) =>
+            this.VisitNode ( node.InnerExpression );
+
+        public virtual void VisitBinaryOperation ( BinaryOperationExpression node )
+        {
+            this.VisitNode ( node.Left );
+            this.VisitNode ( node.Right );
+        }
+
+        public virtual void VisitTableField ( TableField node )
+        {
+            this.VisitNode ( node.Key );
+            this.VisitNode ( node.Value );
+        }
+
+        public virtual void VisitTableConstructor ( TableConstructorExpression node )
+        {
+            foreach ( TableField field in node.Fields )
+            {
+                this.VisitTableField ( field );
+            }
+        }
+
+        public virtual void VisitAnonymousFunction ( AnonymousFunctionExpression node )
+        {
+            foreach ( Expression argument in node.Arguments )
+            {
+                this.VisitNode ( argument );
+            }
+
+            this.VisitStatementList ( node.Body );
+        }
+
+        public virtual void VisitAssignment ( AssignmentStatement node )
+        {
+            foreach ( Expression variable in node.Variables )
+            {
+                this.VisitNode ( variable );
+            }
+
+            foreach ( Expression value in node.Values )
+            {
+                this.VisitNode ( value );
+            }
+        }
+
+        public virtual void VisitBreak ( BreakStatement node )
+        {
+        }
+
+        public virtual void VisitContinue ( ContinueStatement node )
+        {
+        }
+
+        public virtual void VisitDo ( DoStatement node ) =>
+            this.VisitStatementList ( node.Body );
+
+        public virtual void VisitExpressionStatement ( ExpressionStatement node ) =>
+            this.VisitNode ( node.Expression );
+
         public virtual void VisitFunctionDefinition ( FunctionDefinitionStatement node )
         {
             this.VisitNode ( node.Name );
             foreach ( Expression argument in node.Arguments )
+            {
                 this.VisitNode ( argument );
+            }
             this.VisitStatementList ( node.Body );
         }
 
@@ -69,55 +123,81 @@ namespace Loretta.Parsing.Visitor
         {
         }
 
-        public virtual void VisitGroupedExpression ( GroupedExpression node ) => this.VisitNode ( node.InnerExpression );
-
-        public virtual void VisitIdentifier ( IdentifierExpression node )
-        {
-        }
-
         public virtual void VisitIfStatement ( IfStatement node )
         {
             foreach ( IfClause clause in node.Clauses )
-                this.VisitNode ( clause );
-            this.VisitStatementList ( node.ElseBlock );
-        }
+            {
+                this.VisitNode ( clause.Condition );
+                this.VisitStatementList ( clause.Body );
+            }
 
-        public virtual void VisitIndex ( IndexExpression node )
-        {
-            this.VisitNode ( node.Indexee );
-            this.VisitNode ( node.Indexer );
+            if ( node.ElseBlock != null )
+            {
+                this.VisitStatementList ( node.ElseBlock );
+            }
         }
 
         public virtual void VisitGenericFor ( GenericForLoopStatement node )
         {
             foreach ( IdentifierExpression variable in node.Variables )
+            {
                 this.VisitIdentifier ( variable );
+            }
+
+            this.VisitNode ( node.Iteratable );
+            this.VisitStatementList ( node.Body );
         }
 
-        public virtual void VisitLocalVariableDeclaration ( LocalVariableDeclarationStatement node ) => throw new NotImplementedException ( );
+        public virtual void VisitLocalVariableDeclaration ( LocalVariableDeclarationStatement node )
+        {
+            foreach ( IdentifierExpression identifier in node.Identifiers )
+            {
+                this.VisitIdentifier ( identifier );
+            }
 
-        public virtual void VisitNil ( NilExpression node ) => throw new NotImplementedException ( );
+            foreach ( Expression value in node.Values )
+            {
+                this.VisitNode ( value );
+            }
+        }
 
-        public virtual void VisitNumber ( NumberExpression node ) => throw new NotImplementedException ( );
+        public virtual void VisitNode ( LuaASTNode node ) =>
+            node?.Accept ( this );
 
-        public virtual void VisitNumericFor ( NumericForLoopStatement node ) => throw new NotImplementedException ( );
+        public virtual void VisitNumericFor ( NumericForLoopStatement node )
+        {
+            this.VisitIdentifier ( node.Variable );
+            this.VisitNode ( node.Initial );
+            this.VisitNode ( node.Final );
+            if ( node.Step != null )
+                this.VisitNode ( node.Step );
+            this.VisitStatementList ( node.Body );
+        }
 
-        public virtual void VisitRepeatUntil ( RepeatUntilStatement node ) => throw new NotImplementedException ( );
+        public virtual void VisitRepeatUntil ( RepeatUntilStatement node )
+        {
+            this.VisitStatementList ( node.Body );
+            this.VisitNode ( node.Condition );
+        }
 
-        public virtual void VisitReturn ( ReturnStatement node ) => throw new NotImplementedException ( );
+        public virtual void VisitReturn ( ReturnStatement node )
+        {
+            foreach ( Expression value in node.Values )
+                this.VisitNode ( value );
+        }
 
-        public virtual void VisitStatementList ( StatementList node ) => throw new NotImplementedException ( );
+        public virtual void VisitStatementList ( StatementList node )
+        {
+            foreach ( Statement statement in node.Body )
+            {
+                this.VisitNode ( statement );
+            }
+        }
 
-        public virtual void VisitString ( StringExpression node ) => throw new NotImplementedException ( );
-
-        public virtual void VisitTableConstructor ( TableConstructorExpression node ) => throw new NotImplementedException ( );
-
-        public virtual void VisitTableField ( TableField node ) => throw new NotImplementedException ( );
-
-        public virtual void VisitUnaryOperation ( UnaryOperationExpression node ) => throw new NotImplementedException ( );
-
-        public virtual void VisitVarArg ( VarArgExpression node ) => throw new NotImplementedException ( );
-
-        public virtual void VisitWhileLoop ( WhileLoopStatement node ) => throw new NotImplementedException ( );
+        public virtual void VisitWhileLoop ( WhileLoopStatement node )
+        {
+            this.VisitNode ( node.Condition );
+            this.VisitStatementList ( node.Body );
+        }
     }
 }
