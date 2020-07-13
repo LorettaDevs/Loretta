34f885a7fd9956f5a89143794411cde28647dc92
GGG KILLER <gggkiller2@gmail.com>
2020-07-13 17:14:07 -0300

    Add compound assignment node

---
+++ Loretta/Parsing/AST/CompoundAssignmentStatement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GParse.Lexing;
+using Loretta.Lexing;
+using Loretta.Parsing.Visitor;
+
+namespace Loretta.Parsing.AST
+{
+    public class CompoundAssignmentStatement : Statement
+    {
+        public Expression Assignee { get; }
+
+        public Token<LuaTokenType> OperatorToken { get; }
+
+        public Expression ValueExpression { get; }
+
+        public CompoundAssignmentStatement ( Expression assignee, Token<LuaTokenType> operatorToken, Expression valueExpression )
+        {
+            this.Assignee = assignee;
+            this.OperatorToken = operatorToken;
+            this.ValueExpression = valueExpression;
+
+            this.Tokens = this.Assignee.Tokens.Concat ( new[] { this.OperatorToken } )
+                                              .Concat ( this.ValueExpression.Tokens );
+        }
+
+        public override IEnumerable<Token<LuaTokenType>> Tokens { get; }
+
+        public override IEnumerable<LuaASTNode> Children
+        {
+            get
+            {
+                yield return this.Assignee;
+                yield return this.ValueExpression;
+            }
+        }
+
+        internal override void Accept ( ITreeVisitor visitor ) => visitor.VisitCompoundAssignmentStatement ( this );
+
+        internal override T Accept<T> ( ITreeVisitor<T> visitor ) => visitor.VisitCompoundAssignmentStatement ( this );
+    }
+}
