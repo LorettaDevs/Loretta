7f02c04e5c7d4a4aa4b15e4bccf9236215bc46c9
GGG KILLER <gggkiller2@gmail.com>
2020-08-23 18:46:05 -0300

    Add tests for #3

---
+++ Loretta.Tests/Parsing/Visitor/ConstantFolderTests.cs
@@ -0,0 +1,28 @@
+﻿using GParse.Lexing;
+using Loretta.Lexing;
+using Loretta.Parsing;
+using Loretta.Parsing.AST;
+using Loretta.Parsing.Visitor;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Loretta.Tests.Parsing.Visitor
+{
+    [TestClass]
+    public class ConstantFolderTests
+    {
+        private static readonly ConstantFolder constantFolder = new ConstantFolder ( );
+
+        // Regression test for https://github.com/GGG-KILLER/Loretta/issues/3
+        [TestMethod]
+        public void ConstantFolder_FoldsNotExpressionsCorrectly ( )
+        {
+            IdentifierExpression identifier = ASTNodeFactory.Identifier ( "var" );
+            Token<LuaTokenType> not = TokenFactory.Token ( "not", LuaTokenType.Keyword, "not", "not" );
+            UnaryOperationExpression unary = ASTNodeFactory.UnaryOperation ( not, identifier );
+
+            LuaASTNode folded = constantFolder.VisitUnaryOperation ( unary );
+
+            Assert.AreSame ( unary, folded, "Constant folder has folded a not expression with an identifier." );
+        }
+    }
+}
