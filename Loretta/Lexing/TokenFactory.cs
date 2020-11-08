a526db92265f5b1d52f2680d643eb0ecc4ea1799
GGG KILLER <gggkiller2@gmail.com>
2020-11-08 20:04:43 -0300

    Improve the token factory.

--- Loretta/Lexing/TokenFactory.cs
+++ Loretta/Lexing/TokenFactory.cs
@@ -31,15 +31,15 @@ namespace Loretta.Lexing
         /// The location range of the token. If null uses <see cref="SourceRange.Zero" />.
         /// </param>
         /// <returns>The newly created token.</returns>
-        public static LuaToken Token ( String id, LuaTokenType type, String? raw = null, Object? value = null, SourceRange? range = null ) =>
-            new LuaToken ( id, raw ?? id, value, type, range ?? SourceRange.Zero );
+        public static LuaToken Token ( String id, LuaTokenType type, String? raw = null, Object? value = null, SourceRange? range = null, Boolean isTrivia = false ) =>
+            new LuaToken ( id, raw ?? id, value, type, range ?? SourceRange.Zero, isTrivia );
 
         /// <summary>
         /// Creates an identifier token.
         /// </summary>
         /// <param name="identifier">The identifier.</param>
         /// <param name="range">
-        /// <inheritdoc cref="Token(String, LuaTokenType, String?, Object?, SourceRange?)" />
+        /// <inheritdoc cref="Token(String, LuaTokenType, String?, Object?, SourceRange?, Boolean)" />
         /// </param>
         /// <returns>The identifier token.</returns>
         public static LuaToken Identifier ( String identifier, SourceRange? range = null ) =>
@@ -50,7 +50,7 @@ namespace Loretta.Lexing
         /// </summary>
         /// <param name="value">The token's value.</param>
         /// <param name="range">
-        /// <inheritdoc cref="Token(String, LuaTokenType, String?, Object?, SourceRange?)" />
+        /// <inheritdoc cref="Token(String, LuaTokenType, String?, Object?, SourceRange?, Boolean)" />
         /// </param>
         /// <returns>The boolean literal token.</returns>
         public static LuaToken Boolean ( Boolean value, SourceRange? range = null )
@@ -69,7 +69,7 @@ namespace Loretta.Lexing
         /// cref="CultureInfo.InvariantCulture" />.
         /// </param>
         /// <param name="range">
-        /// <inheritdoc cref="Token(String, LuaTokenType, String?, Object?, SourceRange?)" />
+        /// <inheritdoc cref="Token(String, LuaTokenType, String?, Object?, SourceRange?, Boolean)" />
         /// </param>
         /// <returns>The number literal token.</returns>
         public static LuaToken Number ( Double value, String? rawValue = null, SourceRange? range = null )
@@ -89,7 +89,7 @@ namespace Loretta.Lexing
         /// <param name="value">The string's value.</param>
         /// <param name="rawValue">The string's raw (string) form.</param>
         /// <param name="range">
-        /// <inheritdoc cref="Token(String, LuaTokenType, String?, Object?, SourceRange?)" />
+        /// <inheritdoc cref="Token(String, LuaTokenType, String?, Object?, SourceRange?, Boolean)" />
         /// </param>
         /// <returns>The string literal token.</returns>
         public static LuaToken ShortString ( String value, String rawValue, SourceRange? range = null )
@@ -108,7 +108,7 @@ namespace Loretta.Lexing
         /// <param name="value">The string's value.</param>
         /// <param name="rawValue">The string's raw (string) value.</param>
         /// <param name="range">
-        /// <inheritdoc cref="Token(String, LuaTokenType, String?, Object?, SourceRange?)" />
+        /// <inheritdoc cref="Token(String, LuaTokenType, String?, Object?, SourceRange?, Boolean)" />
         /// </param>
         /// <returns>The long string literal token.</returns>
         public static LuaToken LongString ( String value, String rawValue, SourceRange? range = null )
@@ -119,6 +119,87 @@ namespace Loretta.Lexing
                 throw new ArgumentNullException ( nameof ( rawValue ) );
 
             return Token ( "long-string", LuaTokenType.LongString, rawValue, value, range );
+        }
+
+        /// <summary>
+        /// Creates a long string literal token.
+        /// </summary>
+        /// <param name="value">The string's value.</param>
+        /// <param name="rawValue">The string's raw (string) value.</param>
+        /// <param name="start">The string's starting location.</param>
+        /// <returns>The long string literal token.</returns>
+        public static LuaToken LongString ( String value, String rawValue, SourceLocation? start = null )
+        {
+            if ( value is null )
+                throw new ArgumentNullException ( nameof ( value ) );
+            if ( rawValue is null )
+                throw new ArgumentNullException ( nameof ( rawValue ) );
+            SourceRange? range = null;
+            if ( start is SourceLocation s )
+            {
+                var lines = rawValue.Split ( new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries );
+                if ( lines.Length == 1 )
+                {
+                    range = s.To ( new SourceLocation ( s.Line, s.Column + lines[0].Length, s.Byte + rawValue.Length ) );
+                }
+                else
+                {
+                    range = s.To ( new SourceLocation ( s.Line + lines.Length - 1, 1 + lines[^1].Length, s.Byte + rawValue.Length ) );
+                }
+            }
+
+            return Token ( "long-string", LuaTokenType.LongString, rawValue, value, range );
+        }
+
+        /// <summary>
+        /// Creates a single-line comment.
+        /// </summary>
+        /// <param name="value">The comment's contents.</param>
+        /// <param name="rawValue">The comment's raw form.</param>
+        /// <param name="start">The comment's start location.</param>
+        /// <returns></returns>
+        public static LuaToken Comment ( String value, String? rawValue = null, SourceLocation? start = null )
+        {
+            if ( value is null )
+                throw new ArgumentNullException ( nameof ( value ) );
+            if ( value.Contains ( '\r', StringComparison.Ordinal ) || value.Contains ( '\n', StringComparison.Ordinal ) )
+                throw new ArgumentException ( "A single-line comment cannot have line breaks.", nameof ( value ) );
+            rawValue ??= ( "--" + value );
+            SourceRange? range = null;
+            if ( start is SourceLocation s )
+                range = start.Value.To ( new SourceLocation ( s.Line, s.Column + rawValue.Length, s.Byte + rawValue.Length ) );
+
+            return Token ( "comment", LuaTokenType.Comment, rawValue, value, range, true );
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="LuaTokenType.LongComment"/> comment.
+        /// </summary>
+        /// <param name="value">The comment's contents.</param>
+        /// <param name="rawValue">The comment's raw form.</param>
+        /// <param name="start">The starting location of the token.</param>
+        /// <returns></returns>
+        public static LuaToken LongComment ( String value, String rawValue, SourceLocation? start = null )
+        {
+            if ( value is null )
+                throw new ArgumentNullException ( nameof ( value ) );
+            if ( rawValue is null )
+                throw new ArgumentNullException ( nameof ( rawValue ) );
+            SourceRange? range = null;
+            if ( start is SourceLocation s )
+            {
+                var lines = rawValue.Split ( new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries );
+                if ( lines.Length == 1 )
+                {
+                    range = s.To ( new SourceLocation ( s.Line, s.Column + lines[0].Length, s.Byte + rawValue.Length ) );
+                }
+                else
+                {
+                    range = s.To ( new SourceLocation ( s.Line + lines.Length - 1, 1 + lines[^1].Length, s.Byte + rawValue.Length ) );
+                }
+            }
+
+            return Token ( "long-comment", LuaTokenType.LongComment, rawValue, value, range, true );
         }
     }
 }
