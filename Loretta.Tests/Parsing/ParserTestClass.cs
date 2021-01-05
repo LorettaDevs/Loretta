c6e0e5df8e22640fed8d7770ec6e0a472a1cfd2f
GGG KILLER <gggkiller2@gmail.com>
2021-01-05 15:51:50 -0300

    Add regression tests for #17 and #19.

---
+++ Loretta.Tests/Parsing/ParserTestClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GParse.Collections;
+using GParse.Lexing;
+using Loretta.Lexing;
+using Loretta.Parsing;
+using Loretta.Parsing.AST;
+
+namespace Loretta.Tests.Parsing
+{
+    public abstract class ParserTestClass
+    {
+        protected static (StatementList statements, DiagnosticList diagnostics) Parse ( LuaOptions luaOptions, String code )
+        {
+            var diagnostics = new DiagnosticList ( );
+            var lexerBuilder = new LuaLexerBuilder ( luaOptions );
+            var parserBuilder = new LuaParserBuilder ( luaOptions );
+            ILexer<LuaTokenType> lexer = lexerBuilder.CreateLexer ( code, diagnostics );
+            LuaParser parser = parserBuilder.CreateParser ( new TokenReader<LuaTokenType> ( lexer ), diagnostics );
+            return (parser.Parse ( ), diagnostics);
+        }
+    }
+}
