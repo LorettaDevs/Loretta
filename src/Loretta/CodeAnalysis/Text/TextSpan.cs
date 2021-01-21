ddb2f7d2cc6456e7f168661ae9dcb0f7129d3f8e
GGG KILLER <gggkiller2@gmail.com>
2021-01-21 16:28:08 -0300

    Implement TextSpan

---
+++ src/Loretta/CodeAnalysis/Text/TextSpan.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Loretta.CodeAnalysis.Text
+{
+    /// <summary>
+    /// Represents a span of the source text.
+    /// </summary>
+    public readonly struct TextSpan
+    {
+        /// <summary>
+        /// Creates a <see cref="TextSpan"/> from its bounds.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public static TextSpan FromBounds ( Int32 start, Int32 end )
+        {
+            var length = end - start;
+            return new TextSpan ( start, length );
+        }
+
+        /// <summary>
+        /// Initializes a new text span.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="length"></param>
+        public TextSpan ( Int32 start, Int32 length )
+        {
+            this.Start = start;
+            this.Length = length;
+        }
+
+        /// <summary>
+        /// The Span's starting position.
+        /// </summary>
+        public Int32 Start { get; }
+
+        /// <summary>
+        /// The Span's length.
+        /// </summary>
+        public Int32 Length { get; }
+
+        /// <summary>
+        /// The Span's ending position.
+        /// </summary>
+        public Int32 End => this.Start + this.Length;
+
+        /// <summary>
+        /// Checks whether this span overlaps with another.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public Boolean OverlapsWith ( TextSpan other ) =>
+            this.Start < other.End && other.Start < this.End;
+
+        /// <inheritdoc/>
+        public override String ToString ( ) => $"{this.Start}..{this.End}";
+    }
+}
