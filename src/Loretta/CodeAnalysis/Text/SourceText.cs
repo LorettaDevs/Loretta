61863e8a9166ab7eeb512da29aba0a54bdef3841
GGG KILLER <gggkiller2@gmail.com>
2021-01-24 19:34:20 -0300

    Add line parsing to SourceText.

--- src/Loretta/CodeAnalysis/Text/SourceText.cs
+++ src/Loretta/CodeAnalysis/Text/SourceText.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Immutable;
+using System.IO;
 using GParse.IO;
-using GParse.Math;
 
 namespace Loretta.CodeAnalysis.Text
 {
@@ -18,6 +19,72 @@ namespace Loretta.CodeAnalysis.Text
         public static SourceText From ( String text, String fileName = "" ) =>
             new ( text, fileName );
 
+        /// <summary>
+        /// Loads the text from the provided path.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static SourceText Load ( String path )
+        {
+            var text = File.ReadAllText ( path );
+            return From ( text, path );
+        }
+
+        private static void AddLine (
+            ImmutableArray<TextLine>.Builder result,
+            SourceText sourceText,
+            Int32 position,
+            Int32 lineStart,
+            Int32 lineBreakWidth )
+        {
+            var lineLength = position - lineStart;
+            var lineLengthWithLineBreak = lineLength + lineBreakWidth;
+            var line = new TextLine ( sourceText, lineStart, lineLength, lineLengthWithLineBreak );
+            result.Add ( line );
+        }
+
+        private static ImmutableArray<TextLine> ParseLines ( SourceText sourceText, String text )
+        {
+            ImmutableArray<TextLine>.Builder result = ImmutableArray.CreateBuilder<TextLine> ( );
+
+            var position = 0;
+            var lineStart = 0;
+
+            while ( position < text.Length )
+            {
+                switch ( text[position] )
+                {
+                    case '\r':
+                        if ( position + 1 < text.Length && text[position + 1] == '\n' )
+                        {
+                            AddLine ( result, sourceText, position, lineStart, 2 );
+                            position += 2;
+                            lineStart = position;
+                            break;
+                        }
+                        else
+                        {
+                            goto case '\n';
+                        }
+
+                    case '\n':
+                        AddLine ( result, sourceText, position, lineStart, 1 );
+                        position += 1;
+                        lineStart = position;
+                        break;
+
+                    default:
+                        position++;
+                        break;
+                }
+            }
+
+            if ( position >= lineStart )
+                AddLine ( result, sourceText, position, lineStart, 0 );
+
+            return result.ToImmutable ( );
+        }
+
         private readonly String _text;
 
         private SourceText ( String text, String fileName )
@@ -30,6 +97,55 @@ namespace Loretta.CodeAnalysis.Text
         /// The name of the file that originated this text.
         /// </summary>
         public String FileName { get; }
+
+        /// <summary>
+        /// The lines contained within this source text.
+        /// </summary>
+        public ImmutableArray<TextLine> Lines { get; }
+
+        /// <summary>
+        /// The length of this source text.
+        /// </summary>
+        public Int32 Length => this._text.Length;
+
+        /// <summary>
+        /// Obtains the character at the provided <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public Char this[Int32 index] => this._text[index];
+
+        /// <summary>
+        /// Returns the index of the <see cref="TextLine"/> that the
+        /// provided <paramref name="position"/> is contained within.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public Int32 GetLineIndex ( Int32 position )
+        {
+            var lower = 0;
+            var upper = this.Lines.Length - 1;
+
+            while ( lower <= upper )
+            {
+                var index = lower + ( ( upper - lower ) >> 1 );
+                var start = this.Lines[index].Start;
+
+                if ( position == start )
+                    return index;
+
+                if ( start > position )
+                {
+                    upper = index - 1;
+                }
+                else
+                {
+                    lower = index + 1;
+                }
+            }
+
+            return lower - 1;
+        }
 
         /// <summary>
         /// Creates a new reader from this source text.
---
+++ src/Loretta/CodeAnalysis/Text/TextLine.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Loretta.CodeAnalysis.Text
+{
+    /// <summary>
+    /// A line of text in a <see cref="SourceText"/>.
+    /// </summary>
+    public readonly struct TextLine
+    {
+        /// <summary>
+        /// Initializes a new text line.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="start"></param>
+        /// <param name="length"></param>
+        /// <param name="lengthWithLineBreak"></param>
+        internal TextLine ( SourceText text, Int32 start, Int32 length, Int32 lengthWithLineBreak )
+        {
+            this.Text = text;
+            this.Start = start;
+            this.Length = length;
+            this.LengthWithLineBreak = lengthWithLineBreak;
+        }
+
+        /// <summary>
+        /// The <see cref="SourceText"/> this line belongs to.
+        /// </summary>
+        public SourceText Text { get; }
+
+        /// <summary>
+        /// The line's start.
+        /// </summary>
+        public Int32 Start { get; }
+
+        /// <summary>
+        /// The line's length.
+        /// </summary>
+        public Int32 Length { get; }
+
+        /// <summary>
+        /// The line's end.
+        /// </summary>
+        public Int32 End => this.Start + this.Length;
+
+        /// <summary>
+        /// The line's end including the line break.
+        /// </summary>
+        public Int32 LengthWithLineBreak { get; }
+
+        /// <summary>
+        /// This line's span.
+        /// </summary>
+        public TextSpan Span => new TextSpan ( this.Start, this.Length );
+
+        /// <summary>
+        /// This line's span with the line break.
+        /// </summary>
+        public TextSpan SpanWithLineBreak => new TextSpan ( this.Start, this.LengthWithLineBreak );
+
+        /// <summary>
+        /// Retrieves line's contents.
+        /// </summary>
+        /// <returns></returns>
+        public override String ToString ( ) => this.Text.ToString ( this.Span );
+    }
+}
