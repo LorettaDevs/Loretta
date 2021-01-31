94752f3b827eb0094034875c9e363e300e34cb69
GGG KILLER <gggkiller2@gmail.com>
2021-01-30 22:17:39 -0300

    Add the syntax walker.

---
+++ src/Loretta/CodeAnalysis/Syntax/SyntaxWalker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loretta.CodeAnalysis.Syntax
+{
+    /// <summary>
+    /// The base class for algorithms that walk over nodes.
+    /// </summary>
+    public abstract class SyntaxWalker : SyntaxVisitor
+    {
+        public override void VisitToken ( SyntaxToken token )
+        {
+        }
+
+        public virtual void VisitTrivia ( SyntaxTrivia trivia )
+        {
+        }
+    }
+}
