e8b03af1095dc4aaadc5583fb91ec157bb591b0f
GGG KILLER <gggkiller2@gmail.com>
2021-01-31 02:59:36 -0300

    Fix GetChildren generator.

--- src/Loretta.Generators/SyntaxNodes/SyntaxNodeGetChildrenGenerator.cs
+++ src/Loretta.Generators/SyntaxNodes/SyntaxNodeGetChildrenGenerator.cs
@@ -1,5 +1,4 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
@@ -40,94 +39,100 @@ namespace Loretta.Generators.SyntaxNodes
                 writer.WriteLine ( "using System;" );
                 writer.WriteLine ( "using System.Collections.Generic;" );
                 writer.WriteLine ( "using System.Collections.Immutable;" );
+                writer.WriteLine ( "using Tsu;" );
+                writer.WriteLine ( );
+                writer.WriteLine ( "#nullable enable" );
                 writer.WriteLine ( );
 
                 foreach ( INamedTypeSymbol? type in syntaxNodeTypes )
                 {
                     CompletePartialType ( writer, type, writer =>
                     {
-                        IEnumerable<IPropertySymbol> properties = Utilities.AncestorsAndSelf ( type, syntaxNodeType )
-                                                                           .SelectMany ( type => type.GetMembers ( )
-                                                                                                     .OfType<IPropertySymbol> ( ) );
-                        foreach ( IPropertySymbol? property in properties )
+                        using ( new CurlyIndenter ( writer, "public override IEnumerable<SyntaxNode> GetChildren ( )" ) )
                         {
-                            if ( property.Type is INamedTypeSymbol propertyType )
+                            IEnumerable<IPropertySymbol> properties = Utilities.AncestorsAndSelf ( type, syntaxNodeType )
+                                                                               .SelectMany ( type => type.GetMembers ( )
+                                                                                                         .OfType<IPropertySymbol> ( ) );
+                            foreach ( IPropertySymbol? property in properties )
                             {
-                                var isOptional = SymbolEqualityComparer.Default.Equals (
-                                    propertyType.OriginalDefinition,
-                                    optionType );
-                                INamedTypeSymbol actualType = isOptional
-                                                              ? ( INamedTypeSymbol ) propertyType.TypeArguments[0]
-                                                              : propertyType;
-
-                                if ( Utilities.IsDerivedFrom ( actualType, syntaxNodeType ) )
+                                if ( property.Type is INamedTypeSymbol propertyType )
                                 {
-                                    var canBeNull = property.NullableAnnotation == NullableAnnotation.Annotated;
-                                    if ( isOptional && canBeNull )
-                                    {
-                                        writer.WriteLine ( $"if ( {property.Name}.IsSome && {property.Name}.Value != null )" );
-                                        writer.Indent++;
-                                    }
-                                    else if ( isOptional )
-                                    {
-                                        writer.WriteLine ( $"if ( {property.Name}.IsSome )" );
-                                        writer.Indent++;
-                                    }
-                                    else if ( canBeNull )
-                                    {
-                                        writer.WriteLine ( $"if ( {property.Name} != null )" );
-                                        writer.Indent++;
-                                    }
+                                    var isOptional = SymbolEqualityComparer.Default.Equals (
+                                        propertyType.OriginalDefinition,
+                                        optionType );
+                                    INamedTypeSymbol actualType = isOptional
+                                                                  ? ( INamedTypeSymbol ) propertyType.TypeArguments[0]
+                                                                  : propertyType;
 
-                                    if ( isOptional )
-                                        writer.WriteLine ( $"yield return this.{property.Name}.Value;" );
-                                    else
-                                        writer.WriteLine ( $"yield return this.{property.Name};" );
-
-                                    if ( isOptional || canBeNull )
-                                        writer.Indent--;
-                                }
-                                else if ( SymbolEqualityComparer.Default.Equals ( actualType.OriginalDefinition, immutableArrayType )
-                                          && Utilities.IsDerivedFrom ( actualType.TypeArguments[0], syntaxNodeType ) )
-                                {
-                                    CurlyIndenter? indenter = null;
-                                    if ( isOptional )
+                                    if ( Utilities.IsDerivedFrom ( actualType, syntaxNodeType ) )
                                     {
-                                        indenter = new CurlyIndenter (
-                                            writer,
-                                            $"if ( {property.Name}.IsSome )" );
-                                        writer.WriteLine ( $"foreach ( var child in {property.Name}.Value )" );
+                                        var canBeNull = property.NullableAnnotation == NullableAnnotation.Annotated;
+                                        if ( isOptional && canBeNull )
+                                        {
+                                            writer.WriteLine ( $"if ( this.{property.Name}.IsSome && this.{property.Name}.Value != null )" );
+                                            writer.Indent++;
+                                        }
+                                        else if ( isOptional )
+                                        {
+                                            writer.WriteLine ( $"if ( this.{property.Name}.IsSome )" );
+                                            writer.Indent++;
+                                        }
+                                        else if ( canBeNull )
+                                        {
+                                            writer.WriteLine ( $"if ( this.{property.Name} != null )" );
+                                            writer.Indent++;
+                                        }
+
+                                        if ( isOptional )
+                                            writer.WriteLine ( $"yield return this.{property.Name}.Value;" );
+                                        else
+                                            writer.WriteLine ( $"yield return this.{property.Name};" );
+
+                                        if ( isOptional || canBeNull )
+                                            writer.Indent--;
                                     }
-                                    else
+                                    else if ( SymbolEqualityComparer.Default.Equals ( actualType.OriginalDefinition, immutableArrayType )
+                                              && Utilities.IsDerivedFrom ( actualType.TypeArguments[0], syntaxNodeType ) )
                                     {
-                                        writer.WriteLine ( $"foreach ( var child in {property.Name} )" );
+                                        CurlyIndenter? indenter = null;
+                                        if ( isOptional )
+                                        {
+                                            indenter = new CurlyIndenter (
+                                                writer,
+                                                $"if ( this.{property.Name}.IsSome )" );
+                                            writer.WriteLine ( $"foreach ( var child in this.{property.Name}.Value )" );
+                                        }
+                                        else
+                                        {
+                                            writer.WriteLine ( $"foreach ( var child in this.{property.Name} )" );
+                                        }
+
+                                        using ( new Indenter ( writer ) )
+                                            writer.WriteLine ( "yield return child;" );
+
+                                        indenter?.Dispose ( );
                                     }
-
-                                    using ( new Indenter ( writer ) )
-                                        writer.WriteLine ( "yield return child;" );
-
-                                    indenter?.Dispose ( );
-                                }
-                                else if ( SymbolEqualityComparer.Default.Equals ( actualType.OriginalDefinition, separatedSyntaxListType )
-                                          && Utilities.IsDerivedFrom ( actualType.TypeArguments[0], syntaxNodeType ) )
-                                {
-                                    CurlyIndenter? indenter = null;
-                                    if ( isOptional )
+                                    else if ( SymbolEqualityComparer.Default.Equals ( actualType.OriginalDefinition, separatedSyntaxListType )
+                                              && Utilities.IsDerivedFrom ( actualType.TypeArguments[0], syntaxNodeType ) )
                                     {
-                                        indenter = new CurlyIndenter (
-                                            writer,
-                                            $"if ( {property.Name}.IsSome )" );
-                                        writer.WriteLine ( $"foreach ( var child in {property.Name}.Value.GetWithSeparators ( ) )" );
-                                    }
-                                    else
-                                    {
-                                        writer.WriteLine ( $"foreach ( var child in {property.Name}.GetWithSeparators ( ) )" );
-                                    }
+                                        CurlyIndenter? indenter = null;
+                                        if ( isOptional )
+                                        {
+                                            indenter = new CurlyIndenter (
+                                                writer,
+                                                $"if ( this.{property.Name}.IsSome )" );
+                                            writer.WriteLine ( $"foreach ( var child in this.{property.Name}.Value.GetWithSeparators ( ) )" );
+                                        }
+                                        else
+                                        {
+                                            writer.WriteLine ( $"foreach ( var child in this.{property.Name}.GetWithSeparators ( ) )" );
+                                        }
 
-                                    using ( new Indenter ( writer ) )
-                                        writer.WriteLine ( "yield return child;" );
+                                        using ( new Indenter ( writer ) )
+                                            writer.WriteLine ( "yield return child;" );
 
-                                    indenter?.Dispose ( );
+                                        indenter?.Dispose ( );
+                                    }
                                 }
                             }
                         }
