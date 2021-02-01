f6eac779b95a812a50f2708b86a5272a716f7e02
GGG KILLER <gggkiller2@gmail.com>
2021-02-01 16:45:38 -0300

    Add the AnonymousFunctionExpression factory methods.

--- src/Loretta/CodeAnalysis/Syntax/AnonymousFunctionExpressionSyntax.cs
+++ src/Loretta/CodeAnalysis/Syntax/AnonymousFunctionExpressionSyntax.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
 
 namespace Loretta.CodeAnalysis.Syntax
 {
@@ -41,5 +42,60 @@ namespace Loretta.CodeAnalysis.Syntax
         /// The 'end' keyword.
         /// </summary>
         public SyntaxToken EndKeyword { get; }
+    }
+
+    public static partial class SyntaxFactory
+    {
+        /// <summary>
+        /// Creates a new <see cref="AnonymousFunctionExpressionSyntax"/>.
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public static AnonymousFunctionExpressionSyntax AnonymousFunctionExpression (
+            ParameterListSyntax parameters,
+            ImmutableArray<StatementSyntax> body )
+        {
+            if ( parameters is null )
+                throw new ArgumentNullException ( nameof ( parameters ) );
+            if ( body.IsDefault )
+                throw new ArgumentException ( $"'{nameof ( body )}' must not be a default array." );
+
+            return AnonymousFunctionExpression (
+                Token ( SyntaxKind.FunctionKeyword ),
+                parameters,
+                body,
+                Token ( SyntaxKind.EndKeyword ) );
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="AnonymousFunctionExpressionSyntax"/>.
+        /// </summary>
+        /// <param name="functionKeyword"></param>
+        /// <param name="parameters"></param>
+        /// <param name="body"></param>
+        /// <param name="endKeyword"></param>
+        /// <returns></returns>
+        public static AnonymousFunctionExpressionSyntax AnonymousFunctionExpression (
+            SyntaxToken functionKeyword,
+            ParameterListSyntax parameters,
+            ImmutableArray<StatementSyntax> body,
+            SyntaxToken endKeyword )
+        {
+            if ( functionKeyword is null )
+                throw new ArgumentNullException ( nameof ( functionKeyword ) );
+            if ( functionKeyword.Kind != SyntaxKind.FunctionKeyword )
+                throw new ArgumentException ( $"'{nameof ( functionKeyword )}' must be a FunctionKeyword." );
+            if ( parameters is null )
+                throw new ArgumentNullException ( nameof ( parameters ) );
+            if ( body.IsDefault )
+                throw new ArgumentException ( $"'{nameof ( body )}' must not be a default array." );
+            if ( endKeyword is null )
+                throw new ArgumentNullException ( nameof ( endKeyword ) );
+            if ( endKeyword.Kind != SyntaxKind.EndKeyword )
+                throw new ArgumentException ( $"'{nameof ( endKeyword )}' must be an EndKeyword." );
+
+            return new AnonymousFunctionExpressionSyntax ( functionKeyword, parameters, body, endKeyword );
+        }
     }
 }
