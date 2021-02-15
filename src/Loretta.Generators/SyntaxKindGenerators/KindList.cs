bf86042b7d3d73dbce0e3833a542c785e6e02d3a
GGG KILLER <gggkiller2@gmail.com>
2021-02-15 17:23:17 -0300

    Refactor KindInfo obtaining process into utility class and add KindList.

---
+++ src/Loretta.Generators/SyntaxKindGenerators/KindList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Loretta.Generators.SyntaxKindGenerators
+{
+    internal class KindList : IReadOnlyList<KindInfo>
+    {
+        private readonly ImmutableArray<KindInfo> _kinds;
+        private readonly IImmutableDictionary<String, KindInfo> _kindMap;
+
+        public KindList ( ImmutableArray<KindInfo> kinds )
+        {
+
+            this._kinds = kinds;
+            this._kindMap = kinds.ToImmutableDictionary ( kind => kind.Field.Name, StringComparer.Ordinal );
+        }
+
+        #region IReadOnlyList<KindInfo>
+        public KindInfo this[Int32 index] => ( ( IReadOnlyList<KindInfo> ) this._kinds )[index];
+        public Int32 Count => ( ( IReadOnlyCollection<KindInfo> ) this._kinds ).Count;
+        public IEnumerator<KindInfo> GetEnumerator ( ) => ( ( IEnumerable<KindInfo> ) this._kinds ).GetEnumerator ( );
+        IEnumerator IEnumerable.GetEnumerator ( ) => ( ( IEnumerable ) this._kinds ).GetEnumerator ( );
+        #endregion IReadOnlyList<KindInfo>
+
+        #region Dictionary
+        public KindInfo this[String key] => this._kindMap[key];
+        public Boolean ContainsKind ( String key ) => this._kindMap.ContainsKey ( key );
+        public Boolean TryGetKind ( String key, out KindInfo value ) => this._kindMap.TryGetValue ( key, out value );
+        #endregion Dictionary
+
+        public Boolean IsToken ( String kindName ) =>
+            this.TryGetKind ( kindName, out KindInfo kind ) && kind.TokenInfo is not null;
+
+        public Boolean IsAutoCreatableToken ( String kindName ) =>
+            this.TryGetKind ( kindName, out KindInfo kind )
+            && !String.IsNullOrWhiteSpace ( kind.TokenInfo?.Text );
+    }
+}
---
+++ src/Loretta.Generators/SyntaxKindGenerators/KindUtils.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Loretta.Generators.SyntaxKindGenerators
+{
+    internal static class KindUtils
+    {
+        public static KindList? GetKindInfos (
+            GeneratorExecutionContext context,
+            CSharpCompilation compilation )
+        {
+            INamedTypeSymbol? syntaxKindType =
+                compilation.GetTypeByMetadataName ( "Loretta.CodeAnalysis.Syntax.SyntaxKind" );
+
+            if ( syntaxKindType is null )
+                return null;
+
+            var fields = syntaxKindType.GetMembers ( )
+                                   .OfType<IFieldSymbol> ( )
+                                   .ToImmutableArray ( );
+
+            return new KindList ( MapToKindInfo ( context, fields ) );
+        }
+
+        private static ImmutableArray<KindInfo> MapToKindInfo (
+            GeneratorExecutionContext context,
+            IEnumerable<IFieldSymbol> fields )
+        {
+            var compilation = ( CSharpCompilation ) context.Compilation;
+
+            INamedTypeSymbol? triviaAttributeType =
+                compilation.GetTypeByMetadataName ( "Loretta.CodeAnalysis.Syntax.TriviaAttribute" );
+            INamedTypeSymbol? tokenAttributeType =
+                compilation.GetTypeByMetadataName ( "Loretta.CodeAnalysis.Syntax.TokenAttribute" );
+            INamedTypeSymbol? keywordAttributeType =
+                compilation.GetTypeByMetadataName ( "Loretta.CodeAnalysis.Syntax.KeywordAttribute" );
+            INamedTypeSymbol? unaryOperatorAttributeType =
+                compilation.GetTypeByMetadataName ( "Loretta.CodeAnalysis.Syntax.UnaryOperatorAttribute" );
+            INamedTypeSymbol? binaryOperatorAttributeType =
+                compilation.GetTypeByMetadataName ( "Loretta.CodeAnalysis.Syntax.BinaryOperatorAttribute" );
+            INamedTypeSymbol? extraCategoriesAttributeType =
+                compilation.GetTypeByMetadataName ( "Loretta.CodeAnalysis.Syntax.ExtraCategoriesAttribute" );
+            INamedTypeSymbol? propertyAttributeType =
+                compilation.GetTypeByMetadataName ( "Loretta.CodeAnalysis.Syntax.PropertyAttribute" );
+
+            if ( triviaAttributeType is null
+                 || tokenAttributeType is null
+                 || keywordAttributeType is null
+                 || unaryOperatorAttributeType is null
+                 || binaryOperatorAttributeType is null
+                 || extraCategoriesAttributeType is null
+                 || propertyAttributeType is null )
+            {
+                return ImmutableArray<KindInfo>.Empty;
+            }
+
+            var fieldsArray = fields.ToImmutableArray ( );
+            ImmutableArray<KindInfo>.Builder infos = ImmutableArray.CreateBuilder<KindInfo> ( fieldsArray.Length );
+            foreach ( IFieldSymbol field in fieldsArray )
+            {
+                var isTrivia = IsTrivia ( triviaAttributeType, field );
+                TokenInfo? tokenInfo =
+                    GetTokenInfo ( tokenAttributeType, keywordAttributeType, field );
+                OperatorInfo? unaryOperatorInfo =
+                    GetOperatorInfo ( unaryOperatorAttributeType, field );
+                OperatorInfo? binaryOperatorInfo =
+                    GetOperatorInfo ( binaryOperatorAttributeType, field );
+                ImmutableArray<String> extraCategories =
+                    GetExtraCategories ( extraCategoriesAttributeType, field );
+                ImmutableDictionary<String, TypedConstant> properties =
+                    GetProperties ( propertyAttributeType, field );
+
+                var hasErrors = false;
+                Location location = field.Locations.Single ( );
+                if ( isTrivia && tokenInfo is not null )
+                {
+                    hasErrors = true;
+                    context.ReportDiagnostic ( Diagnostic.Create ( Diagnostics.TriviaKindIsAlsoAToken, location ) );
+                }
+
+                if ( tokenInfo is { IsKeyword: true, Text: null } )
+                {
+                    hasErrors = true;
+                    context.ReportDiagnostic ( Diagnostic.Create ( Diagnostics.KeywordKindWithoutText, location ) );
+                }
+
+                if ( ( unaryOperatorInfo is not null || binaryOperatorInfo is not null ) && String.IsNullOrWhiteSpace ( tokenInfo?.Text ) )
+                {
+                    hasErrors = true;
+                    context.ReportDiagnostic ( Diagnostic.Create ( Diagnostics.OperatorKindWithoutText, location ) );
+                }
+
+                if ( hasErrors )
+                    continue;
+
+                infos.Add ( new KindInfo (
+                    field,
+                    isTrivia,
+                    tokenInfo,
+                    unaryOperatorInfo,
+                    binaryOperatorInfo,
+                    extraCategories,
+                    properties ) );
+            }
+
+            return infos.ToImmutable ( );
+        }
+
+        private static Boolean IsTrivia ( INamedTypeSymbol triviaAttributeType, IFieldSymbol field ) =>
+            Utilities.GetAttribute ( field, triviaAttributeType ) is not null;
+
+        private static TokenInfo? GetTokenInfo (
+            INamedTypeSymbol tokenAttributeType,
+            INamedTypeSymbol keywordAttributeType,
+            IFieldSymbol field )
+        {
+            if ( Utilities.GetAttribute ( field, keywordAttributeType ) is AttributeData keywordAttributeData )
+            {
+                var text = keywordAttributeData.ConstructorArguments.Single ( ).Value as String;
+                if ( String.IsNullOrWhiteSpace ( text ) ) text = null;
+                return new TokenInfo ( text, true );
+            }
+            else if ( Utilities.GetAttribute ( field, tokenAttributeType ) is AttributeData tokenAttributeData )
+            {
+                var text = tokenAttributeData.NamedArguments.SingleOrDefault ( kv => kv.Key == "Text" ).Value.Value as String;
+                if ( String.IsNullOrWhiteSpace ( text ) ) text = null;
+                return new TokenInfo ( text, false );
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private static OperatorInfo? GetOperatorInfo (
+            INamedTypeSymbol operatorAttributeType,
+            IFieldSymbol field )
+        {
+            AttributeData? attr = Utilities.GetAttribute ( field, operatorAttributeType );
+            if ( attr is null )
+                return null;
+
+            var precedence = ( Int32 ) attr.ConstructorArguments[0].Value!;
+            TypedConstant expression = attr.ConstructorArguments[1];
+            return new OperatorInfo ( precedence, expression );
+        }
+
+        private static ImmutableArray<String> GetExtraCategories ( INamedTypeSymbol extraCategoriesAttributeType, IFieldSymbol field )
+        {
+            AttributeData? attr = Utilities.GetAttribute ( field, extraCategoriesAttributeType );
+            if ( attr is null )
+                return ImmutableArray<String>.Empty;
+
+            var categories = attr.ConstructorArguments.Single ( ).Values.Select ( arg => ( String ) arg.Value! ).ToImmutableArray ( );
+            return categories;
+        }
+
+        private static ImmutableDictionary<String, TypedConstant> GetProperties ( INamedTypeSymbol propertyAttributeType, IFieldSymbol field )
+        {
+            ImmutableArray<AttributeData> attributes = Utilities.GetAttributes ( field, propertyAttributeType );
+            if ( attributes.IsEmpty )
+                return ImmutableDictionary<String, TypedConstant>.Empty;
+
+            IEnumerable<KeyValuePair<String, TypedConstant>> properties = attributes.Select ( attr => new KeyValuePair<String, TypedConstant> ( ( String ) attr.ConstructorArguments[0].Value!, attr.ConstructorArguments[1] ) );
+            return ImmutableDictionary.CreateRange ( properties );
+        }
+    }
+}
