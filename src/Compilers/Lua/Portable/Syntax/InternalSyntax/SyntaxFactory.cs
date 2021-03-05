d780121786845b32875ecd32304df48ee42c50e8
GGG KILLER <gggkiller2@gmail.com>
2021-03-05 20:41:23 -0300

    Add in the rest of the SyntaxFactory methods.

--- src/Compilers/Lua/Portable/Syntax/InternalSyntax/SyntaxFactory.cs
+++ src/Compilers/Lua/Portable/Syntax/InternalSyntax/SyntaxFactory.cs
@@ -1,4 +1,7 @@
-﻿namespace Loretta.CodeAnalysis.Lua.Syntax.InternalSyntax
+﻿using System;
+using Loretta.Utilities;
+
+namespace Loretta.CodeAnalysis.Lua.Syntax.InternalSyntax
 {
     internal static partial class SyntaxFactory
     {
@@ -62,5 +65,59 @@
 
             return trivia.WithAnnotationsGreen(new[] { SyntaxAnnotation.ElasticAnnotation });
         }
+
+        internal static SyntaxTrivia Comment(string text)
+        {
+            return isLongComment(text)
+                ? SyntaxTrivia.Create(SyntaxKind.MultiLineCommentTrivia, text)
+                : SyntaxTrivia.Create(SyntaxKind.SingleLineCommentTrivia, text);
+
+            static bool isLongComment(string text)
+            {
+                if (text.StartsWith("/*", StringComparison.Ordinal))
+                    return true;
+                if (text.StartsWith("--[", StringComparison.Ordinal))
+                {
+                    var offset = 3;
+                    while (text[offset] == '=') offset++;
+                    return text[offset] == '[';
+                }
+                return false;
+            }
+        }
+
+        public static SyntaxToken Token(SyntaxKind kind) => SyntaxToken.Create(kind);
+
+        internal static SyntaxToken Token(GreenNode? leading, SyntaxKind kind, GreenNode? trailing) => SyntaxToken.Create(kind, leading, trailing);
+
+        internal static SyntaxToken Token(GreenNode? leading, SyntaxKind kind, string text, string valueText, GreenNode? trailing)
+        {
+            RoslynDebug.Assert(SyntaxFacts.IsToken(kind));
+            RoslynDebug.Assert(kind != SyntaxKind.IdentifierToken);
+            RoslynDebug.Assert(kind != SyntaxKind.NumericLiteralToken);
+
+            var defaultText = SyntaxFacts.GetText(kind);
+            return kind >= SyntaxToken.FirstTokenWithWellKnownText && kind <= SyntaxToken.LastTokenWithWellKnownText && text == defaultText && valueText == defaultText
+                ? Token(leading, kind, trailing)
+                : SyntaxToken.WithValue(kind, leading, text, valueText, trailing);
+        }
+
+        internal static SyntaxToken MissingToken(SyntaxKind kind) => SyntaxToken.CreateMissing(kind, null, null);
+
+        internal static SyntaxToken MissingToken(GreenNode? leading, SyntaxKind kind, GreenNode? trailing) => SyntaxToken.CreateMissing(kind, leading, trailing);
+
+        internal static SyntaxToken Identifier(string text) => Identifier(SyntaxKind.IdentifierToken, null, text, text, null);
+
+        internal static SyntaxToken Identifier(GreenNode? leading, string text, GreenNode? trailing) => Identifier(SyntaxKind.IdentifierToken, leading, text, text, trailing);
+
+        internal static SyntaxToken Identifier(SyntaxKind contextualKind, GreenNode? leading, string text, string valueText, GreenNode? trailing) => SyntaxToken.Identifier(contextualKind, leading, text, valueText, trailing);
+
+        internal static SyntaxToken Literal(GreenNode? leading, string text, double value, GreenNode? trailing) => SyntaxToken.WithValue(SyntaxKind.NumericLiteralToken, leading, text, value, trailing);
+
+        internal static SyntaxToken Literal(GreenNode? leading, string text, string value, GreenNode? trailing) => SyntaxToken.WithValue(SyntaxKind.StringLiteralToken, leading, text, value, trailing);
+
+        internal static SyntaxToken Literal(GreenNode? leading, string text, SyntaxKind kind, string value, GreenNode? trailing) => SyntaxToken.WithValue(kind, leading, text, value, trailing);
+
+        internal static SyntaxToken BadToken(GreenNode? leading, string text, GreenNode? trailing) => SyntaxToken.WithValue(SyntaxKind.BadToken, leading, text, text, trailing);
     }
 }
