4fccfe7b933c50e2da8b83b48ff7f37f2ee213d5
GGG KILLER <gggkiller2@gmail.com>
2021-03-05 20:40:16 -0300

    Implement the rest of LuaSyntaxRewriter and Visitors.

--- src/Compilers/Lua/Portable/Syntax/InternalSyntax/LuaSyntaxRewriter.cs
+++ src/Compilers/Lua/Portable/Syntax/InternalSyntax/LuaSyntaxRewriter.cs
@@ -1,10 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Loretta.CodeAnalysis.Lua.Syntax.InternalSyntax
+﻿namespace Loretta.CodeAnalysis.Lua.Syntax.InternalSyntax
 {
+    using Loretta.CodeAnalysis.Syntax.InternalSyntax;
+    using Loretta.Utilities;
+
     internal abstract partial class LuaSyntaxRewriter : LuaSyntaxVisitor<LuaSyntaxNode>
     {
+        protected bool VisitIntoStructuredTrivia { get; }
+
+        public LuaSyntaxRewriter(bool visitIntoStructuredTrivia = false)
+        {
+            VisitIntoStructuredTrivia = visitIntoStructuredTrivia;
+        }
+
+        public override LuaSyntaxNode VisitToken(SyntaxToken token)
+        {
+            var leading = VisitList(token.LeadingTrivia);
+            var trailing = VisitList(token.TrailingTrivia);
+
+            if (leading != token.LeadingTrivia || trailing != token.TrailingTrivia)
+            {
+                if (leading != token.LeadingTrivia)
+                {
+                    token = token.TokenWithLeadingTrivia(leading.Node);
+                }
+
+                if (trailing != token.TrailingTrivia)
+                {
+                    token = token.TokenWithTrailingTrivia(trailing.Node);
+                }
+            }
+
+            return token;
+        }
+
+        public SyntaxList<TNode> VisitList<TNode>(SyntaxList<TNode> list) where TNode : LuaSyntaxNode
+        {
+            SyntaxListBuilder? alternate = null;
+            for (int i = 0, n = list.Count; i < n; i++)
+            {
+                var item = list[i];
+                var visited = Visit(item);
+                if (item != visited && alternate == null)
+                {
+                    alternate = new SyntaxListBuilder(n);
+                    alternate.AddRange(list, 0, i);
+                }
+
+                if (alternate != null)
+                {
+                    RoslynDebug.Assert(visited != null && visited.Kind != SyntaxKind.None, "Cannot remove node using Syntax.InternalSyntax.SyntaxRewriter.");
+                    alternate.Add(visited);
+                }
+            }
+
+            if (alternate != null)
+            {
+                return alternate.ToList();
+            }
+
+            return list;
+        }
+
+        public SeparatedSyntaxList<TNode> VisitList<TNode>(SeparatedSyntaxList<TNode> list) where TNode : LuaSyntaxNode
+        {
+            // A separated list is filled with Lua nodes and Lua tokens.  Both of which
+            // derive from InternalSyntax.LuaSyntaxNode.  So this cast is appropriately
+            // typesafe.
+            var withSeps = (SyntaxList<LuaSyntaxNode>) list.GetWithSeparators();
+            var result = VisitList(withSeps);
+            if (result != withSeps)
+            {
+                return result.AsSeparatedList<TNode>();
+            }
+
+            return list;
+        }
     }
 }
--- src/Compilers/Lua/Portable/Syntax/InternalSyntax/LuaSyntaxVisitor.cs
+++ src/Compilers/Lua/Portable/Syntax/InternalSyntax/LuaSyntaxVisitor.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Loretta.CodeAnalysis.Lua.Syntax.InternalSyntax
+﻿namespace Loretta.CodeAnalysis.Lua.Syntax.InternalSyntax
 {
     internal abstract partial class LuaSyntaxVisitor<TResult>
     {
-        public virtual TResult Visit(LuaSyntaxNode node)
+        public virtual TResult? Visit(LuaSyntaxNode? node)
             => node is null ? default : node.Accept(this);
+
+        public virtual TResult? VisitToken(SyntaxToken token) => DefaultVisit(token);
+
+        public virtual TResult? VisitTrivia(SyntaxTrivia trivia) => DefaultVisit(trivia);
+
+        protected virtual TResult? DefaultVisit(LuaSyntaxNode node) => default;
+    }
+
+    internal abstract partial class LuaSyntaxVisitor
+    {
+        public virtual void Visit(LuaSyntaxNode node)
+        {
+            if (node is null)
+                return;
+            node.Accept(this);
+        }
+
+        public virtual void VisitToken(SyntaxToken token) => DefaultVisit(token);
+
+        public virtual void VisitTrivia(SyntaxTrivia trivia) => DefaultVisit(trivia);
+
+        protected virtual void DefaultVisit(LuaSyntaxNode node) { }
     }
 }
