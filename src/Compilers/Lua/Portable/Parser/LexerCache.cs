3a04ed0994d94192d43be88c6c74e5580964ed53
GGG KILLER <gggkiller2@gmail.com>
2021-03-09 14:09:28 -0300

    Add the lexer cache.

---
+++ src/Compilers/Lua/Portable/Parser/LexerCache.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Loretta.CodeAnalysis.PooledObjects;
+using Loretta.Utilities;
+
+namespace Loretta.CodeAnalysis.Lua.Syntax.InternalSyntax
+{
+    internal sealed class LexerCache
+    {
+        private static readonly ObjectPool<CachingIdentityFactory<string, SyntaxKind>> s_keywordKindPool =
+            CachingIdentityFactory<string, SyntaxKind>.CreatePool(512, key => SyntaxFacts.GetKeywordKind(key));
+        private readonly TextKeyedCache<SyntaxTrivia> _triviaMap;
+        private readonly CachingIdentityFactory<string, SyntaxKind> _keywordKindMap;
+
+        internal LexerCache()
+        {
+            _triviaMap = TextKeyedCache<SyntaxTrivia>.GetInstance();
+            _keywordKindMap = s_keywordKindPool.Allocate();
+        }
+
+        internal void Free()
+        {
+            _keywordKindMap.Free();
+            _triviaMap.Free();
+        }
+
+        internal bool TryGetKeywordKind(string key, out SyntaxKind kind)
+        {
+            if (key.Length < SyntaxFacts.MinKeywordLength || key.Length > SyntaxFacts.MaxKeywordLength)
+            {
+                kind = SyntaxKind.IdentifierToken;
+                return false;
+            }
+
+            kind = _keywordKindMap.GetOrMakeValue(key);
+            return kind != SyntaxKind.IdentifierToken;
+        }
+
+        internal SyntaxTrivia LookupTrivia(
+            string key,
+            int hashCode,
+            Func<SyntaxTrivia> createTriviaFunction)
+        {
+            var value = _triviaMap.FindItem(key.AsSpan(), hashCode);
+
+            if (value == null)
+            {
+                value = createTriviaFunction();
+                _triviaMap.AddItem(key.AsSpan(), hashCode, value);
+            }
+
+            return value;
+        }
+    }
+}
