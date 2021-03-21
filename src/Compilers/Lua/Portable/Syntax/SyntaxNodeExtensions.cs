73be92f3d5a9b47bbebfc0741036935714bea0bf
GGG KILLER <gggkiller2@gmail.com>
2021-03-21 02:42:24 -0300

    Add the SyntaxNodeExtensions class.

---
+++ src/Compilers/Lua/Portable/Syntax/SyntaxNodeExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Loretta.CodeAnalysis.Lua
+{
+    internal static class SyntaxNodeExtensions
+    {
+        public static TNode WithAnnotations<TNode>(this TNode node, params SyntaxAnnotation[] annotations) where TNode : LuaSyntaxNode =>
+            (TNode) node.Green.SetAnnotations(annotations).CreateRed(node.Parent, node.Position);
+    }
+}
