4cef50a3af86bcb458d88bc2bad23328d6e02a02
GGG KILLER <gggkiller2@gmail.com>
2021-03-22 14:13:33 -0300

    Add the public structured trivia node.

---
+++ src/Compilers/Lua/Portable/Syntax/StructuredTriviaSyntax.cs
@@ -0,0 +1,34 @@
+﻿using Loretta.Utilities;
+
+namespace Loretta.CodeAnalysis.Lua.Syntax
+{
+    /// <summary>
+    /// It's a non terminal Trivia CSharpSyntaxNode that has a tree underneath it.
+    /// </summary>
+    public abstract partial class StructuredTriviaSyntax : LuaSyntaxNode, IStructuredTriviaSyntax
+    {
+        private SyntaxTrivia _parent;
+
+        internal StructuredTriviaSyntax(InternalSyntax.LuaSyntaxNode green, SyntaxNode parent, int position)
+            : base(green, position, parent?.SyntaxTree)
+        {
+            System.Diagnostics.Debug.Assert(parent == null || position >= 0);
+        }
+
+        internal static StructuredTriviaSyntax Create(SyntaxTrivia trivia)
+        {
+            var node = trivia.UnderlyingNode;
+            RoslynDebug.Assert(node is not null);
+            var parent = trivia.Token.Parent;
+            var position = trivia.Position;
+            var red = (StructuredTriviaSyntax) node.CreateRed(parent, position);
+            red._parent = trivia;
+            return red;
+        }
+
+        /// <summary>
+        /// Get parent trivia.
+        /// </summary>
+        public override SyntaxTrivia ParentTrivia => _parent;
+    }
+}
