6aab1e18084d4f7039c0ca6ba8d90be9ccf3eee0
GGG KILLER <gggkiller2@gmail.com>
2021-03-23 20:39:50 -0300

    Bring in syntax first/last token replacer.

---
+++ src/Compilers/Lua/Portable/Syntax/InternalSyntax/SyntaxFirstTokenReplacer.cs
@@ -0,0 +1,78 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable disable
+
+using System.Diagnostics;
+
+namespace Loretta.CodeAnalysis.Lua.Syntax.InternalSyntax
+{
+    internal class SyntaxFirstTokenReplacer : LuaSyntaxRewriter
+    {
+        private readonly SyntaxToken _oldToken;
+        private readonly SyntaxToken _newToken;
+        private readonly int _diagnosticOffsetDelta;
+        private bool _foundOldToken;
+
+        private SyntaxFirstTokenReplacer(SyntaxToken oldToken, SyntaxToken newToken, int diagnosticOffsetDelta)
+        {
+            _oldToken = oldToken;
+            _newToken = newToken;
+            _diagnosticOffsetDelta = diagnosticOffsetDelta;
+            _foundOldToken = false;
+        }
+
+        internal static TRoot Replace<TRoot>(TRoot root, SyntaxToken oldToken, SyntaxToken newToken, int diagnosticOffsetDelta)
+            where TRoot : LuaSyntaxNode
+        {
+            var replacer = new SyntaxFirstTokenReplacer(oldToken, newToken, diagnosticOffsetDelta);
+            var newRoot = (TRoot) replacer.Visit(root);
+            Debug.Assert(replacer._foundOldToken);
+            return newRoot;
+        }
+
+        public override LuaSyntaxNode Visit(LuaSyntaxNode node)
+        {
+            if (node != null)
+            {
+                if (!_foundOldToken)
+                {
+                    if (node is SyntaxToken token)
+                    {
+                        Debug.Assert(token == _oldToken);
+                        _foundOldToken = true;
+                        return _newToken; // NB: diagnostic offsets have already been updated (by SyntaxParser.AddSkippedSyntax)
+                    }
+
+                    return UpdateDiagnosticOffset(base.Visit(node), _diagnosticOffsetDelta);
+                }
+            }
+
+            return node;
+        }
+
+        private static TSyntax UpdateDiagnosticOffset<TSyntax>(TSyntax node, int diagnosticOffsetDelta) where TSyntax : LuaSyntaxNode
+        {
+            var oldDiagnostics = node.GetDiagnostics();
+            if (oldDiagnostics == null || oldDiagnostics.Length == 0)
+            {
+                return node;
+            }
+
+            var numDiagnostics = oldDiagnostics.Length;
+            var newDiagnostics = new DiagnosticInfo[numDiagnostics];
+            for (var i = 0; i < numDiagnostics; i++)
+            {
+                var oldDiagnostic = oldDiagnostics[i];
+                newDiagnostics[i] = oldDiagnostic is not SyntaxDiagnosticInfo oldSyntaxDiagnostic ?
+                    oldDiagnostic :
+                    new SyntaxDiagnosticInfo(
+                        oldSyntaxDiagnostic.Offset + diagnosticOffsetDelta,
+                        oldSyntaxDiagnostic.Width,
+                        (ErrorCode) oldSyntaxDiagnostic.Code,
+                        oldSyntaxDiagnostic.Arguments);
+            }
+            return node.WithDiagnosticsGreen(newDiagnostics);
+        }
+    }
+}
---
+++ src/Compilers/Lua/Portable/Syntax/InternalSyntax/SyntaxLastTokenReplacer.cs
@@ -0,0 +1,59 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable disable
+
+using System.Diagnostics;
+
+namespace Loretta.CodeAnalysis.Lua.Syntax.InternalSyntax
+{
+    internal class SyntaxLastTokenReplacer : LuaSyntaxRewriter
+    {
+        private readonly SyntaxToken _oldToken;
+        private readonly SyntaxToken _newToken;
+        private int _count = 1;
+        private bool _found;
+
+        private SyntaxLastTokenReplacer(SyntaxToken oldToken, SyntaxToken newToken)
+        {
+            _oldToken = oldToken;
+            _newToken = newToken;
+        }
+
+        internal static TRoot Replace<TRoot>(TRoot root, SyntaxToken newToken)
+            where TRoot : LuaSyntaxNode
+        {
+            var oldToken = root.GetLastToken();
+            var replacer = new SyntaxLastTokenReplacer(oldToken, newToken);
+            var newRoot = (TRoot) replacer.Visit(root);
+            Debug.Assert(replacer._found);
+            return newRoot;
+        }
+
+        private static int CountNonNullSlots(LuaSyntaxNode node) =>
+            node.ChildNodesAndTokens().Count;
+
+        public override LuaSyntaxNode Visit(LuaSyntaxNode node)
+        {
+            if (node != null && !_found)
+            {
+                _count--;
+                if (_count == 0)
+                {
+                    if (node is SyntaxToken token)
+                    {
+                        Debug.Assert(token == _oldToken);
+                        _found = true;
+                        return _newToken;
+                    }
+
+                    _count += CountNonNullSlots(node);
+                    return base.Visit(node);
+                }
+            }
+
+            return node;
+        }
+    }
+}
