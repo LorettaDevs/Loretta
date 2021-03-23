fd70e466fb44d2b7db410e0d14832066f9ac374b
GGG KILLER <gggkiller2@gmail.com>
2021-03-23 20:17:18 -0300

    Add the (unimplemented) SyntaxNormalizer.

---
+++ src/Compilers/Lua/Portable/Syntax/SyntaxNormalizer.cs
@@ -0,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Loretta.CodeAnalysis.PooledObjects;
+using Loretta.CodeAnalysis.Text;
+
+namespace Loretta.CodeAnalysis.Lua.Syntax
+{
+    internal class SyntaxNormalizer : LuaSyntaxRewriter
+    {
+        private readonly TextSpan _consideredSpan;
+        private readonly int _initialDepth;
+        private readonly string _indentWhitespace;
+        private readonly bool _useElasticTrivia;
+        private readonly SyntaxTrivia _eolTrivia;
+
+        private readonly bool _isInStructuredTrivia;
+
+        private readonly SyntaxToken _previousToken;
+
+        private readonly bool _afterLineBreak;
+        private readonly bool _afterIndentation;
+
+        // CONSIDER: if we become concerned about space, we shouldn't actually need any 
+        // of the values between indentations[0] and indentations[initialDepth] (exclusive).
+        private readonly ArrayBuilder<SyntaxTrivia>? _indentations;
+
+        private SyntaxNormalizer(TextSpan consideredSpan, int initialDepth, string indentWhitespace, string eolWhitespace, bool useElasticTrivia)
+            : base(visitIntoStructuredTrivia: true)
+        {
+            _consideredSpan = consideredSpan;
+            _initialDepth = initialDepth;
+            _indentWhitespace = indentWhitespace;
+            _useElasticTrivia = useElasticTrivia;
+            _eolTrivia = useElasticTrivia ? SyntaxFactory.ElasticEndOfLine(eolWhitespace) : SyntaxFactory.EndOfLine(eolWhitespace);
+            _afterLineBreak = true;
+        }
+
+        internal static TNode Normalize<TNode>(TNode node, string indentWhitespace, string eolWhitespace, bool useElasticTrivia = false)
+            where TNode : SyntaxNode =>
+            throw new NotImplementedException();
+
+        internal static SyntaxToken Normalize(SyntaxToken token, string indentWhitespace, string eolWhitespace, bool useElasticTrivia = false) =>
+            throw new NotImplementedException();
+
+        internal static SyntaxTriviaList Normalize(SyntaxTriviaList trivia, string indentWhitespace, string eolWhitespace, bool useElasticTrivia = false) =>
+            throw new NotImplementedException();
+    }
+}
