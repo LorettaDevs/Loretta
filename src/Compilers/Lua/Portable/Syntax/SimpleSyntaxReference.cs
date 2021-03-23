90cfc31dbe79048517e0579ef5ec35f54b760c83
GGG KILLER <gggkiller2@gmail.com>
2021-03-23 18:43:06 -0300

    Add SimpleSyntaxReference.

---
+++ src/Compilers/Lua/Portable/Syntax/SimpleSyntaxReference.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading;
+using Loretta.CodeAnalysis.Text;
+
+namespace Loretta.CodeAnalysis.Lua
+{
+    /// <summary>
+    /// this is a basic do-nothing implementation of a syntax reference
+    /// </summary>
+    internal class SimpleSyntaxReference : SyntaxReference
+    {
+        private readonly SyntaxNode _node;
+
+        internal SimpleSyntaxReference(SyntaxNode node)
+        {
+            _node = node;
+        }
+
+        public override SyntaxTree SyntaxTree => _node.SyntaxTree;
+
+        public override TextSpan Span => _node.Span;
+
+        public override SyntaxNode GetSyntax(CancellationToken cancellationToken) => _node;
+    }
+}
