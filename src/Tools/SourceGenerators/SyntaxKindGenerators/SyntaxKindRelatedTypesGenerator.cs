0313eca31c99c049a59bbbe6a74966512eec57dd
GGG KILLER <gggkiller2@gmail.com>
2021-03-28 00:10:24 -0300

    Remove exception logging.

--- src/Tools/SourceGenerators/SyntaxKindGenerators/SyntaxKindRelatedTypesGenerator.cs
+++ src/Tools/SourceGenerators/SyntaxKindGenerators/SyntaxKindRelatedTypesGenerator.cs
@@ -26,29 +26,16 @@ namespace Loretta.Generators.SyntaxKindGenerators
             context.AddSource("SyntaxKindAttributes.g.cs", KindUtils.SyntaxKindAttributesText);
             Utilities.DoVsCodeHack(syntaxKindType, "SyntaxKindAttributes.g.cs", KindUtils.SyntaxKindAttributesText);
 
-            try
+            var kinds = KindUtils.GetKindInfos(context, compilation);
+            if (kinds is null)
+                throw new Exception("KindList is null");
+            if (kinds.Count < 1)
             {
-                var kinds = KindUtils.GetKindInfos(context, compilation);
-                if (kinds is null)
-                    throw new Exception("KindList is null");
-                if (kinds.Count < 1)
-                {
-                    context.ReportDiagnostic(Diagnostic.Create(Diagnostics.NoSyntaxKindWithAttributesFound, syntaxKindType.Locations.Single()));
-                    return;
-                }
-
-                GenerateSyntaxFacts(context, syntaxKindType, kinds);
+                context.ReportDiagnostic(Diagnostic.Create(Diagnostics.NoSyntaxKindWithAttributesFound, syntaxKindType.Locations.Single()));
+                return;
             }
-            catch (Exception ex)
-            {
-                var syntaxKindFilePath = syntaxKindType.DeclaringSyntaxReferences.First().SyntaxTree.FilePath;
-                var syntaxDirectory = Path.GetDirectoryName(syntaxKindFilePath);
-                var filePath = Path.Combine(syntaxDirectory, "exception.log");
-                var contents = string.Join(Environment.NewLine, new string('-', 30), ex.ToString());
-                File.AppendAllText(filePath, contents);
 
-                throw;
-            }
+            GenerateSyntaxFacts(context, syntaxKindType, kinds);
         }
     }
 }
