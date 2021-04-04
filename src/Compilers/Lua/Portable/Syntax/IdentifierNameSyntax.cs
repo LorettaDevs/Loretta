656b5e95a3c4a9950829be7a28bd0d45ef242151
GGG KILLER <gggkiller2@gmail.com>
2021-04-04 00:27:45 -0300

    Add Name properties to some nodes.

---
+++ src/Compilers/Lua/Portable/Syntax/IdentifierNameSyntax.cs
@@ -0,0 +1,10 @@
+﻿namespace Loretta.CodeAnalysis.Lua.Syntax
+{
+    public sealed partial class IdentifierNameSyntax
+    {
+        /// <summary>
+        /// This identifier's name.
+        /// </summary>
+        public string Name => Identifier.Text;
+    }
+}
---
+++ src/Compilers/Lua/Portable/Syntax/NamedParameterSyntax.cs
@@ -0,0 +1,10 @@
+﻿namespace Loretta.CodeAnalysis.Lua.Syntax
+{
+    public sealed partial class NamedParameterSyntax
+    {
+        /// <summary>
+        /// This parameter's name.
+        /// </summary>
+        public string Name => Identifier.Text;
+    }
+}
