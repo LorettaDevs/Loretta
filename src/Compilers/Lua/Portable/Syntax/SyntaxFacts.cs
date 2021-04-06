977a56f915910faf183f235d6e6b0087d28428f1
GGG KILLER <gggkiller2@gmail.com>
2021-04-06 15:30:29 -0300

    Move RequiresSeparator to SyntaxFacts.

--- src/Compilers/Lua/Portable/Syntax/SyntaxFacts.cs
+++ src/Compilers/Lua/Portable/Syntax/SyntaxFacts.cs
@@ -1,4 +1,5 @@
-﻿using Tsu;
+﻿using System;
+using Tsu;
 
 namespace Loretta.CodeAnalysis.Lua
 {
@@ -48,6 +49,96 @@ namespace Loretta.CodeAnalysis.Lua
                 SyntaxKind.ContinueKeyword => syntaxOptions.ContinueType == ContinueType.ContextualKeyword,
                 _ => false
             };
+
+        /// <summary>
+        /// Whether two tokens/trivia require a separator between them.
+        /// </summary>
+        /// <param name="kindA"></param>
+        /// <param name="kindAText"></param>
+        /// <param name="kindB"></param>
+        /// <param name="kindBText"></param>
+        /// <returns></returns>
+        internal static bool RequiresSeparator(SyntaxKind kindA, string kindAText, SyntaxKind kindB, string kindBText)
+        {
+            if (kindAText is null)
+                throw new ArgumentNullException(nameof(kindAText));
+            if (kindBText is null)
+                throw new ArgumentNullException(nameof(kindBText));
+
+            var kindAIsKeyword = SyntaxFacts.IsKeyword(kindA);
+            var kindBIsKeyowrd = SyntaxFacts.IsKeyword(kindB);
+
+            if (kindA is SyntaxKind.IdentifierToken && kindB is SyntaxKind.IdentifierToken)
+                return true;
+            if (kindAIsKeyword && kindBIsKeyowrd)
+                return true;
+            if (kindAIsKeyword && kindB is SyntaxKind.IdentifierToken)
+                return true;
+            if (kindA is SyntaxKind.IdentifierToken && kindBIsKeyowrd)
+                return true;
+            if (kindA is SyntaxKind.IdentifierToken && kindB is SyntaxKind.NumericLiteralToken)
+                return true;
+            if (kindA is SyntaxKind.NumericLiteralToken && kindB is SyntaxKind.IdentifierToken)
+                return true;
+            if (kindA is SyntaxKind.NumericLiteralToken && kindBIsKeyowrd)
+                return true;
+            if (kindA is SyntaxKind.NumericLiteralToken && kindB is SyntaxKind.DotToken or SyntaxKind.DotDotToken or SyntaxKind.DotDotDotToken or SyntaxKind.DotDotEqualsToken)
+                return true;
+            if (kindAIsKeyword && kindB is SyntaxKind.NumericLiteralToken)
+                return true;
+            if (kindA is SyntaxKind.NumericLiteralToken && kindB is SyntaxKind.NumericLiteralToken)
+                return true;
+            if (kindA is SyntaxKind.OpenBracketToken && kindB is SyntaxKind.OpenBracketToken)
+                return true;
+            if (kindA is SyntaxKind.OpenBracketToken && kindB == SyntaxKind.StringLiteralToken && kindBText.StartsWith("["))
+                return true;
+            if (kindA is SyntaxKind.ColonToken && kindB is SyntaxKind.ColonToken or SyntaxKind.ColonColonToken)
+                return true;
+            if (kindA is SyntaxKind.PlusToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.MinusToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.MinusToken && kindB is SyntaxKind.SingleLineCommentTrivia or SyntaxKind.MultiLineCommentTrivia && kindBText.StartsWith("-"))
+                return true;
+            if (kindA is SyntaxKind.MinusToken && kindB is SyntaxKind.MinusToken or SyntaxKind.MinusEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.StarToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.SlashToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.SlashEqualsToken or SyntaxKind.EqualsEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.SlashToken && kindB is SyntaxKind.SlashToken or SyntaxKind.StarToken or SyntaxKind.StartEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.SlashToken && kindB is SyntaxKind.SingleLineCommentTrivia or SyntaxKind.MultiLineCommentTrivia && kindBText.StartsWith("/"))
+                return true;
+            if (kindA is SyntaxKind.HatToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.PercentToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.DotDotToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.DotToken or SyntaxKind.DotDotToken && kindB is SyntaxKind.DotToken or SyntaxKind.DotDotToken or SyntaxKind.DotDotDotToken or SyntaxKind.DotDotEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.EqualsToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.BangToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
+                return true;
+            if (kindA is SyntaxKind.LessThanToken && kindB is SyntaxKind.LessThanToken or SyntaxKind.LessThanEqualsToken or SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken or SyntaxKind.LessThanLessThanToken)
+                return true;
+            if (kindA is SyntaxKind.GreaterThanToken && kindB is SyntaxKind.GreaterThanToken or SyntaxKind.GreaterThanEqualsToken or SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken or SyntaxKind.GreaterThanGreaterThanToken)
+                return true;
+            if (kindA is SyntaxKind.AmpersandToken && kindB is SyntaxKind.AmpersandToken or SyntaxKind.AmpersandAmpersandToken)
+                return true;
+            if (kindA is SyntaxKind.PipeToken && kindB is SyntaxKind.PipeToken or SyntaxKind.PipePipeToken)
+                return true;
+            // Dot can be the start of a number
+            if (kindA is SyntaxKind.DotToken or SyntaxKind.DotDotToken or SyntaxKind.DotDotDotToken && kindB is SyntaxKind.NumericLiteralToken)
+                return true;
+            // Shebang
+            if (kindA is SyntaxKind.HashToken && kindB is SyntaxKind.BangToken or SyntaxKind.BangEqualsToken)
+                return true;
+
+            return false;
+        }
 
         /// <summary>
         /// Obtains the kind of the operator of the compound assignment operator.
--- src/Compilers/Lua/Test/Syntax/Lexical/LexicalTestData.cs
+++ src/Compilers/Lua/Test/Syntax/Lexical/LexicalTestData.cs
@@ -196,101 +196,19 @@ aaa
             };
         }
 
-        public static bool RequiresSeparator(SyntaxKind kindA, string kindAText, SyntaxKind kindB, string kindBText)
-        {
-            if (kindAText is null)
-                throw new ArgumentNullException(nameof(kindAText));
-            if (kindBText is null)
-                throw new ArgumentNullException(nameof(kindBText));
-
-            var kindAIsKeyword = SyntaxFacts.IsKeyword(kindA);
-            var kindBIsKeyowrd = SyntaxFacts.IsKeyword(kindB);
-
-            if (kindA is SyntaxKind.IdentifierToken && kindB is SyntaxKind.IdentifierToken)
-                return true;
-            if (kindAIsKeyword && kindBIsKeyowrd)
-                return true;
-            if (kindAIsKeyword && kindB is SyntaxKind.IdentifierToken)
-                return true;
-            if (kindA is SyntaxKind.IdentifierToken && kindBIsKeyowrd)
-                return true;
-            if (kindA is SyntaxKind.IdentifierToken && kindB is SyntaxKind.NumericLiteralToken)
-                return true;
-            if (kindA is SyntaxKind.NumericLiteralToken && kindB is SyntaxKind.IdentifierToken)
-                return true;
-            if (kindA is SyntaxKind.NumericLiteralToken && kindBIsKeyowrd)
-                return true;
-            if (kindA is SyntaxKind.NumericLiteralToken && kindB is SyntaxKind.DotToken or SyntaxKind.DotDotToken or SyntaxKind.DotDotDotToken or SyntaxKind.DotDotEqualsToken)
-                return true;
-            if (kindAIsKeyword && kindB is SyntaxKind.NumericLiteralToken)
-                return true;
-            if (kindA is SyntaxKind.NumericLiteralToken && kindB is SyntaxKind.NumericLiteralToken)
-                return true;
-            if (kindA is SyntaxKind.OpenBracketToken && kindB is SyntaxKind.OpenBracketToken)
-                return true;
-            if (kindA is SyntaxKind.OpenBracketToken && kindB == SyntaxKind.StringLiteralToken && kindBText.StartsWith("["))
-                return true;
-            if (kindA is SyntaxKind.ColonToken && kindB is SyntaxKind.ColonToken or SyntaxKind.ColonColonToken)
-                return true;
-            if (kindA is SyntaxKind.PlusToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.MinusToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.MinusToken && kindB is SyntaxKind.SingleLineCommentTrivia or SyntaxKind.MultiLineCommentTrivia && kindBText.StartsWith("-"))
-                return true;
-            if (kindA is SyntaxKind.MinusToken && kindB is SyntaxKind.MinusToken or SyntaxKind.MinusEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.StarToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.SlashToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.SlashEqualsToken or SyntaxKind.EqualsEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.SlashToken && kindB is SyntaxKind.SlashToken or SyntaxKind.StarToken or SyntaxKind.StartEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.SlashToken && kindB is SyntaxKind.SingleLineCommentTrivia or SyntaxKind.MultiLineCommentTrivia && kindBText.StartsWith("/"))
-                return true;
-            if (kindA is SyntaxKind.HatToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.PercentToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.DotDotToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.DotToken or SyntaxKind.DotDotToken && kindB is SyntaxKind.DotToken or SyntaxKind.DotDotToken or SyntaxKind.DotDotDotToken or SyntaxKind.DotDotEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.EqualsToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.BangToken && kindB is SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken)
-                return true;
-            if (kindA is SyntaxKind.LessThanToken && kindB is SyntaxKind.LessThanToken or SyntaxKind.LessThanEqualsToken or SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken or SyntaxKind.LessThanLessThanToken)
-                return true;
-            if (kindA is SyntaxKind.GreaterThanToken && kindB is SyntaxKind.GreaterThanToken or SyntaxKind.GreaterThanEqualsToken or SyntaxKind.EqualsToken or SyntaxKind.EqualsEqualsToken or SyntaxKind.GreaterThanGreaterThanToken)
-                return true;
-            if (kindA is SyntaxKind.AmpersandToken && kindB is SyntaxKind.AmpersandToken or SyntaxKind.AmpersandAmpersandToken)
-                return true;
-            if (kindA is SyntaxKind.PipeToken && kindB is SyntaxKind.PipeToken or SyntaxKind.PipePipeToken)
-                return true;
-            // Dot can be the start of a number
-            if (kindA is SyntaxKind.DotToken or SyntaxKind.DotDotToken or SyntaxKind.DotDotDotToken && kindB is SyntaxKind.NumericLiteralToken)
-                return true;
-            // Shebang
-            if (kindA is SyntaxKind.HashToken && kindB is SyntaxKind.BangToken or SyntaxKind.BangEqualsToken)
-                return true;
-
-            return false;
-        }
-
         public static IEnumerable<(ShortToken tokenA, ShortToken tokenB)> GetTokenPairs(LuaSyntaxOptions options) =>
             from tokenA in GetTokens(options)
             from tokB in GetTokens(options)
-            where !RequiresSeparator(tokenA.Kind, tokenA.Text, tokB.Kind, tokB.Text)
+            where !SyntaxFacts.RequiresSeparator(tokenA.Kind, tokenA.Text, tokB.Kind, tokB.Text)
             let tokenB = tokB.WithSpan(new TextSpan(tokenA.Span.End, tokB.Span.Length))
             select (tokenA, tokenB);
 
         public static IEnumerable<(ShortToken tokenA, ShortToken separator, ShortToken tokenB)> GetTokenPairsWithSeparators(LuaSyntaxOptions options) =>
             from tokenA in GetTokens(options)
             from tokB in GetTokens(options)
-            where !RequiresSeparator(tokenA.Kind, tokenA.Text, tokB.Kind, tokB.Text)
+            where !SyntaxFacts.RequiresSeparator(tokenA.Kind, tokenA.Text, tokB.Kind, tokB.Text)
             from sep in GetSeparators()
-            where !RequiresSeparator(tokenA.Kind, tokenA.Text, sep.Kind, sep.Text) && !RequiresSeparator(sep.Kind, sep.Text, tokB.Kind, tokB.Text)
+            where !SyntaxFacts.RequiresSeparator(tokenA.Kind, tokenA.Text, sep.Kind, sep.Text) && !SyntaxFacts.RequiresSeparator(sep.Kind, sep.Text, tokB.Kind, tokB.Text)
             let separator = sep.WithSpan(new TextSpan(tokenA.Span.End, sep.Span.Length))
             let tokenB = tokB.WithSpan(new TextSpan(separator.Span.End, tokB.Span.Length))
             select (tokenA, separator, tokenB);
