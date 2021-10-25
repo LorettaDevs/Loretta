4630cccb122a88724a8e075a6ed0a3882ad7d577
GGG KILLER <gggkiller2@gmail.com>
2021-10-25 20:43:57 -0300

    Migrate the ErrorFacts generator to the new incremental system.

--- src/Tools/Loretta.Generators.ErrorFactsGenerator/SourceGenerator.cs
+++ src/Tools/Loretta.Generators.ErrorFactsGenerator/SourceGenerator.cs
@@ -1,99 +1,101 @@
 ﻿using System;
-using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.Text;
 
 namespace Loretta.Generators.ErrorCode
 {
-    [Generator]
-    public sealed class SourceGenerator : GeneratorBase
+    [Generator(LanguageNames.CSharp)]
+    public sealed class SourceGenerator : IIncrementalGenerator
     {
-        protected override void GenerateFiles(GeneratorExecutionContext context, CSharpCompilation compilation)
+        public void Initialize(IncrementalGeneratorInitializationContext context)
         {
-            var errorCodeType =
-                compilation.GetTypeByMetadataName("Loretta.CodeAnalysis.Lua.ErrorCode");
+            var errorCodeType = context.CompilationProvider.Select((comp, _) =>
+                comp.GetTypeByMetadataName("Loretta.CodeAnalysis.Lua.ErrorCode"));
 
-            if (errorCodeType is null) return;
+            var errorCodeFields = errorCodeType.SelectMany((type, _) =>
+                type?.GetMembers().OfType<IFieldSymbol>() ?? Enumerable.Empty<IFieldSymbol>())
+                                               .Collect();
 
-            var codes = errorCodeType.GetMembers().OfType<IFieldSymbol>().ToImmutableArray();
-
-            SourceText sourceText;
-            using (var writer = new SourceWriter())
+            context.RegisterSourceOutput(errorCodeFields, (context, codes) =>
             {
-                using (writer.CurlyIndenter("namespace Loretta.CodeAnalysis.Lua"))
-                using (writer.CurlyIndenter("internal static partial class ErrorFacts"))
+                if (codes.IsEmpty)
                 {
-                    using (writer.CurlyIndenter("public static partial bool IsWarning(ErrorCode code)"))
-                    using (writer.CurlyIndenter("switch(code)"))
-                    {
-                        var warnings = codes.Where(field => field.Name.StartsWith("WRN_", StringComparison.OrdinalIgnoreCase));
-                        if (warnings.Any())
-                        {
-                            foreach (var code in warnings)
-                                writer.WriteLine($"case ErrorCode.{code.Name}:");
-                            using (writer.Indenter())
-                                writer.WriteLine("return true;");
-                        }
-                        writer.WriteLine("default:");
-                        using (writer.Indenter())
-                            writer.WriteLine("return false;");
-                    }
-                    writer.WriteLine();
-                    using (writer.CurlyIndenter("public static partial bool IsFatal(ErrorCode code)"))
-                    using (writer.CurlyIndenter("switch(code)"))
-                    {
-                        var fatals = codes.Where(field => field.Name.StartsWith("FTL_", StringComparison.OrdinalIgnoreCase));
-                        if (fatals.Any())
-                        {
-                            foreach (var code in fatals)
-                                writer.WriteLine($"case ErrorCode.{code.Name}:");
-                            using (writer.Indenter())
-                                writer.WriteLine("return true;");
-                        }
-                        writer.WriteLine("default:");
-                        using (writer.Indenter())
-                            writer.WriteLine("return false;");
-                    }
-                    writer.WriteLine();
-                    using (writer.CurlyIndenter("public static partial bool IsInfo(ErrorCode code)"))
-                    using (writer.CurlyIndenter("switch(code)"))
-                    {
-                        var infos = codes.Where(field => field.Name.StartsWith("INF_", StringComparison.OrdinalIgnoreCase));
-                        if (infos.Any())
-                        {
-                            foreach (var code in infos)
-                                writer.WriteLine($"case ErrorCode.{code.Name}:");
-                            using (writer.Indenter())
-                                writer.WriteLine("return true;");
-                        }
-                        writer.WriteLine("default:");
-                        using (writer.Indenter())
-                            writer.WriteLine("return false;");
-                    }
-                    writer.WriteLine();
-                    using (writer.CurlyIndenter("public static partial bool IsHidden(ErrorCode code)"))
-                    using (writer.CurlyIndenter("switch(code)"))
-                    {
-                        var hidden = codes.Where(field => field.Name.StartsWith("HDN_", StringComparison.OrdinalIgnoreCase));
-                        if (hidden.Any())
-                        {
-                            foreach (var code in hidden)
-                                writer.WriteLine($"case ErrorCode.{code.Name}:");
-                            using (writer.Indenter())
-                                writer.WriteLine("return true;");
-                        }
-                        writer.WriteLine("default:");
-                        using (writer.Indenter())
-                            writer.WriteLine("return false;");
-                    }
+                    return;
                 }
+                else
+                {
+                    using var writer = new SourceWriter();
+                    using (writer.CurlyIndenter("namespace Loretta.CodeAnalysis.Lua"))
+                    using (writer.CurlyIndenter("internal static partial class ErrorFacts"))
+                    {
+                        using (writer.CurlyIndenter("public static partial bool IsWarning(ErrorCode code)"))
+                        using (writer.CurlyIndenter("switch(code)"))
+                        {
+                            var warnings = codes.Where(field => field.Name.StartsWith("WRN_", StringComparison.OrdinalIgnoreCase));
+                            if (warnings.Any())
+                            {
+                                foreach (var code in warnings)
+                                    writer.WriteLine($"case ErrorCode.{code.Name}:");
+                                using (writer.Indenter())
+                                    writer.WriteLine("return true;");
+                            }
+                            writer.WriteLine("default:");
+                            using (writer.Indenter())
+                                writer.WriteLine("return false;");
+                        }
+                        writer.WriteLine();
+                        using (writer.CurlyIndenter("public static partial bool IsFatal(ErrorCode code)"))
+                        using (writer.CurlyIndenter("switch(code)"))
+                        {
+                            var fatals = codes.Where(field => field.Name.StartsWith("FTL_", StringComparison.OrdinalIgnoreCase));
+                            if (fatals.Any())
+                            {
+                                foreach (var code in fatals)
+                                    writer.WriteLine($"case ErrorCode.{code.Name}:");
+                                using (writer.Indenter())
+                                    writer.WriteLine("return true;");
+                            }
+                            writer.WriteLine("default:");
+                            using (writer.Indenter())
+                                writer.WriteLine("return false;");
+                        }
+                        writer.WriteLine();
+                        using (writer.CurlyIndenter("public static partial bool IsInfo(ErrorCode code)"))
+                        using (writer.CurlyIndenter("switch(code)"))
+                        {
+                            var infos = codes.Where(field => field.Name.StartsWith("INF_", StringComparison.OrdinalIgnoreCase));
+                            if (infos.Any())
+                            {
+                                foreach (var code in infos)
+                                    writer.WriteLine($"case ErrorCode.{code.Name}:");
+                                using (writer.Indenter())
+                                    writer.WriteLine("return true;");
+                            }
+                            writer.WriteLine("default:");
+                            using (writer.Indenter())
+                                writer.WriteLine("return false;");
+                        }
+                        writer.WriteLine();
+                        using (writer.CurlyIndenter("public static partial bool IsHidden(ErrorCode code)"))
+                        using (writer.CurlyIndenter("switch(code)"))
+                        {
+                            var hidden = codes.Where(field => field.Name.StartsWith("HDN_", StringComparison.OrdinalIgnoreCase));
+                            if (hidden.Any())
+                            {
+                                foreach (var code in hidden)
+                                    writer.WriteLine($"case ErrorCode.{code.Name}:");
+                                using (writer.Indenter())
+                                    writer.WriteLine("return true;");
+                            }
+                            writer.WriteLine("default:");
+                            using (writer.Indenter())
+                                writer.WriteLine("return false;");
+                        }
+                    }
 
-                sourceText = writer.GetText();
-            }
-
-            context.AddSource("ErrorFacts.g.cs", sourceText);
+                    context.AddSource("ErrorFacts.g.cs", writer.GetText());
+                }
+            });
         }
     }
 }
