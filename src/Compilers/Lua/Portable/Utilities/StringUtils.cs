b1dd12d77737d9306ee4c52b37dc68a51b5605e4
GGG KILLER <gggkiller2@gmail.com>
2021-10-25 21:32:33 -0300

    Add a string-based IsIdentifier overload.
    This is required for TFMs where there's no implicit Span to string support.

--- src/Compilers/Lua/Portable/Utilities/StringUtils.cs
+++ src/Compilers/Lua/Portable/Utilities/StringUtils.cs
@@ -11,11 +11,33 @@ namespace Loretta.CodeAnalysis.Lua.Utilities
         /// Returns whether the provided string is a valid identifier.
         /// </summary>
         /// <param name="value">The string to check.</param>
-        /// 
         /// <returns>Whether the provided string is a valid identifier.</returns>
         public static bool IsIdentifier(ReadOnlySpan<char> value)
         {
             if (value.IsEmpty)
+                return false;
+
+            if (!CharUtils.IsValidFirstIdentifierChar(value[0]))
+                return false;
+
+            for (var idx = 1; idx < value.Length; idx++)
+            {
+                if (!CharUtils.IsValidTrailingIdentifierChar(value[idx]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns whether the provided string is a valid identifier.
+        /// </summary>
+        /// <param name="value">The string to check.</param>
+        ///
+        /// <returns>Whether the provided string is a valid identifier.</returns>
+        public static bool IsIdentifier(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
                 return false;
 
             if (!CharUtils.IsValidFirstIdentifierChar(value[0]))
