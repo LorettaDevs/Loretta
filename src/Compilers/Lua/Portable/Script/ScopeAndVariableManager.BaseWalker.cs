052d084be0bf3a489df1d5d867b3ee9733b8118b
GGG KILLER <gggkiller2@gmail.com>
2021-10-26 14:41:40 -0300

    Add a base walker for scoping walkers.

---
+++ src/Compilers/Lua/Portable/Script/ScopeAndVariableManager.BaseWalker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Loretta.Utilities;
+
+namespace Loretta.CodeAnalysis.Lua
+{
+    internal partial class ScopeAndVariableManager
+    {
+        private class BaseWalker : LuaSyntaxWalker
+        {
+            protected readonly IDictionary<SyntaxNode, IScope> _scopes;
+
+            protected BaseWalker(
+                IDictionary<SyntaxNode, IScope> scopes,
+                SyntaxWalkerDepth depth = SyntaxWalkerDepth.Node)
+                : base(depth)
+            {
+                RoslynDebug.AssertNotNull(scopes);
+
+                _scopes = scopes;
+            }
+
+            protected IScopeInternal? FindScope(SyntaxNode leaf)
+            {
+                foreach (var node in leaf.AncestorsAndSelf())
+                {
+                    if (_scopes.TryGetValue(node, out var scope))
+                        return (IScopeInternal) scope;
+                }
+
+                return null;
+            }
+        }
+    }
+}
