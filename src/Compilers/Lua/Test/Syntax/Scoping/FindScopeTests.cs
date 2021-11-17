d57e53964da7ebb55f8d084b5c15cc89839b5a6b
GGG KILLER <gggkiller2@gmail.com>
2021-11-16 21:54:10 -0300

    Add tests for FileScope fixes.

---
+++ src/Compilers/Lua/Test/Syntax/Scoping/FindScopeTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Xunit;
+
+namespace Loretta.CodeAnalysis.Lua.Syntax.UnitTests.Scoping
+{
+    public class ScopeTests
+    {
+        private static (SyntaxTree, Script) ParseScript(string code)
+        {
+            var tree = LuaSyntaxTree.ParseText("print 'Hello'");
+            var script = new Script(ImmutableArray.Create(tree));
+            return (tree, script);
+        }
+
+        [Fact]
+        public void CompilationUnit_HasFileScope()
+        {
+            var (tree, script) = ParseScript("print 'Hello'");
+
+            var compilationUnit = (CompilationUnitSyntax) tree.GetRoot();
+            var compilationUnitScope = script.GetScope(compilationUnit);
+
+            Assert.NotNull(compilationUnitScope);
+            Assert.Equal(ScopeKind.File, compilationUnitScope.Kind);
+            Assert.Equal(script.RootScope, compilationUnitScope.Parent);
+        }
+
+        [Fact]
+        public void FindScope_OnRootElement_ReturnsRootScope()
+        {
+            var (tree, script) = ParseScript("print 'Hello'");
+
+            var compilationUnit = (CompilationUnitSyntax) tree.GetRoot();
+            var compilationUnitScope = script.GetScope(compilationUnit);
+            var printExpression = (FunctionCallExpressionSyntax) ((ExpressionStatementSyntax) compilationUnit.Statements.Statements.Single()).Expression;
+            var printExpressionScope = script.FindScope(printExpression.Expression);
+
+            Assert.Equal(compilationUnitScope, printExpressionScope);
+        }
+    }
+}
