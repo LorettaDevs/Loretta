5a516811f88d0994d1c05e4761d8d272c53c03ac
GGG KILLER <gggkiller2@gmail.com>
2021-12-21 21:39:52 -0300

    Implement the error codes for if expressions.

--- src/Compilers/Lua/Portable/LuaResources.Designer.cs
+++ src/Compilers/Lua/Portable/LuaResources.Designer.cs
@@ -19,7 +19,7 @@ namespace Loretta.CodeAnalysis.Lua {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class LuaResources {
@@ -192,6 +192,42 @@ namespace Loretta.CodeAnalysis.Lua {
         internal static string ERR_IdentifierExpectedKW {
             get {
                 return ResourceManager.GetString("ERR_IdentifierExpectedKW", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Condition not found for if expression.
+        /// </summary>
+        internal static string ERR_IfExpressionConditionExpected {
+            get {
+                return ResourceManager.GetString("ERR_IfExpressionConditionExpected", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to If expressions require a condition but one was not found, did you perhaps forget to specify one?.
+        /// </summary>
+        internal static string ERR_IfExpressionConditionExpected_Description {
+            get {
+                return ResourceManager.GetString("ERR_IfExpressionConditionExpected_Description", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to If expressions require a condition.
+        /// </summary>
+        internal static string ERR_IfExpressionConditionExpected_Title {
+            get {
+                return ResourceManager.GetString("ERR_IfExpressionConditionExpected_Title", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to If expressions are not supported in this lua version.
+        /// </summary>
+        internal static string ERR_IfExpressionsNotSupportedInLuaVersion {
+            get {
+                return ResourceManager.GetString("ERR_IfExpressionsNotSupportedInLuaVersion", resourceCulture);
             }
         }
         
