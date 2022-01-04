050190b9930c31c30dfc777f8fb1072d333199a1
GGG KILLER <gggkiller2@gmail.com>
2022-01-03 22:35:24 -0300

    Implement the naming strategy delegate.

---
+++ src/Compilers/Lua/Experimental/Minifying/NamingStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace Loretta.CodeAnalysis.Lua.Experimental.Minifying
+{
+    /// <summary>
+    /// The naming strategy to use to convert a slot into a variable
+    /// name.
+    /// Uses the provided scope to check if the variable name is not being
+    /// used already.
+    /// </summary>
+    /// <param name="scope">The scope the slot will be used in.</param>
+    /// <param name="slot">The slot to convert to a variable name.</param>
+    /// <returns></returns>
+    public delegate string NamingStrategy(IScope scope, int slot);
+}
