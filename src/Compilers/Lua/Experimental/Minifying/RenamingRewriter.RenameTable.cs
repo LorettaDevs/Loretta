daadadac6511c61c714899de38e78f8cb5ab4549
GGG KILLER <gggkiller2@gmail.com>
2022-01-03 22:36:33 -0300

    Implement the rewriter that does the actual renaming.

---
+++ src/Compilers/Lua/Experimental/Minifying/RenamingRewriter.RenameTable.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using Loretta.Utilities;
+using System.Linq;
+
+namespace Loretta.CodeAnalysis.Lua.Experimental.Minifying
+{
+    internal partial class RenamingRewriter
+    {
+        private class RenameTable
+        {
+            private readonly object _lock = new();
+            private readonly Script _script;
+            private readonly StateStack _stateStack;
+            private readonly NamingStrategy _namingStrategy;
+            private readonly Dictionary<IVariable, SyntaxNode> _lastUseCache = new();
+            private readonly Dictionary<IVariable, string> _variableRenameMap = new();
+
+            public RenameTable(Script script, StateStack stateStack, NamingStrategy namingStrategy)
+            {
+                _script = script;
+                _stateStack = stateStack;
+                _namingStrategy = namingStrategy;
+            }
+
+            /// <summary>
+            /// Gets the location the variable is last used.
+            /// </summary>
+            /// <param name="variable"></param>
+            /// <returns></returns>
+            public SyntaxNode GetLastUse(IVariable variable)
+            {
+                SyntaxNode? use;
+                lock (_lock)
+                {
+                    if (!_lastUseCache.TryGetValue(variable, out use))
+                    {
+                        _lastUseCache[variable] = use =
+                            variable.ReadLocations.Concat(variable.WriteLocations)
+                                                  .OrderByDescending(node => node.Location.SourceSpan.Start)
+                                                  .First();
+                    }
+                }
+
+                return use;
+            }
+
+            /// <summary>
+            /// Gets the new variable name to be used for this node.
+            /// </summary>
+            /// <param name="node"></param>
+            /// <returns>
+            /// If <see langword="null"/>, remains unchanged.
+            /// </returns>
+            public string? GetNewVariableName(SyntaxNode node)
+            {
+                var variable = _script.GetVariable(node);
+                if (variable is null)
+                    throw ExceptionUtilities.Unreachable;
+                if (variable.Kind is VariableKind.Iteration or VariableKind.Local or VariableKind.Parameter)
+                    return null;
+
+                // Get or calculate the new name for the variable of the
+                // provided node.
+                if (!_variableRenameMap.TryGetValue(variable, out var name))
+                {
+                    var slot = _stateStack.Slot;
+                    name = _namingStrategy(_stateStack.Scope, slot);
+                    _variableRenameMap[variable] = name;
+                    // Increment the slot so the slot we just used is not
+                    // used by another variable.
+                    _stateStack.IncrementSlot();
+                }
+
+                // If this the last use of this variable, then we won't be
+                // needing it for the rest of the code so we can reuse the
+                // number it was using.
+                if (GetLastUse(variable) == node)
+                    _stateStack.DecrementSlot();
+
+                return name;
+            }
+        }
+    }
+}
---
+++ src/Compilers/Lua/Experimental/Minifying/RenamingRewriter.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Loretta.Utilities;
+using System.Threading;
+using System.Linq;
+using Loretta.CodeAnalysis.Lua.Syntax;
+
+namespace Loretta.CodeAnalysis.Lua.Experimental.Minifying
+{
+    internal partial class RenamingRewriter : LuaSyntaxRewriter
+    {
+        private readonly Script _script;
+        private readonly StateStack _stateStack;
+        private readonly RenameTable _renameTable;
+
+        public RenamingRewriter(Script script, NamingStrategy namingStrategy)
+        {
+            _script = script;
+            _stateStack = new StateStack();
+            _renameTable = new RenameTable(script, _stateStack, namingStrategy);
+        }
+
+        #region Scope Pushing
+
+        public override SyntaxNode? VisitCompilationUnit(CompilationUnitSyntax node)
+        {
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return base.VisitCompilationUnit(node);
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        public override SyntaxNode? VisitAnonymousFunctionExpression(AnonymousFunctionExpressionSyntax node)
+        {
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return base.VisitAnonymousFunctionExpression(node);
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        public override SyntaxNode? VisitNumericForStatement(NumericForStatementSyntax node)
+        {
+            // Update the values that are outside of the inner scope first.
+            var initialValue = (ExpressionSyntax) Visit(node.InitialValue);
+            var finalValue = (ExpressionSyntax) Visit(node.FinalValue);
+            var stepValue = node.StepValue;
+            if (stepValue is not null)
+                stepValue = (ExpressionSyntax) Visit(stepValue);
+
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return node.Update(
+                    node.ForKeyword,
+                    (IdentifierNameSyntax?) Visit(node.Identifier) ?? throw new ArgumentNullException("identifier"),
+                    node.EqualsToken,
+                    initialValue,
+                    node.FinalValueCommaToken,
+                    finalValue,
+                    node.StepValueCommaToken,
+                    stepValue,
+                    node.DoKeyword,
+                    (StatementListSyntax?) Visit(node.Body) ?? throw new ArgumentNullException("body"),
+                    node.EndKeyword,
+                    node.SemicolonToken);
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        public override SyntaxNode? VisitGenericForStatement(GenericForStatementSyntax node)
+        {
+            var expressions = VisitList(node.Expressions);
+
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return node.Update(
+                    node.ForKeyword,
+                    VisitList(node.Identifiers),
+                    node.InKeyword,
+                    expressions,
+                    node.DoKeyword,
+                    (StatementListSyntax?) Visit(node.Body) ?? throw new ArgumentNullException("body"),
+                    node.EndKeyword,
+                    node.SemicolonToken);
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        public override SyntaxNode? VisitWhileStatement(WhileStatementSyntax node)
+        {
+            var condition = (ExpressionSyntax) Visit(node.Condition) ?? throw new ArgumentNullException("condition");
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return node.Update(
+                    node.WhileKeyword,
+                    condition,
+                    node.DoKeyword,
+                    (StatementListSyntax?) Visit(node.Body) ?? throw new ArgumentNullException("body"),
+                    node.EndKeyword,
+                    node.SemicolonToken);
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        public override SyntaxNode? VisitRepeatUntilStatement(RepeatUntilStatementSyntax node)
+        {
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return base.VisitRepeatUntilStatement(node);
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        public override SyntaxNode? VisitIfStatement(IfStatementSyntax node)
+        {
+            var condition = (ExpressionSyntax?) Visit(node.Condition) ?? throw new ArgumentNullException("condition");
+
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return node.Update(
+                    node.IfKeyword,
+                    condition,
+                    node.ThenKeyword,
+                    (StatementListSyntax?) Visit(node.Body) ?? throw new ArgumentNullException("body"),
+                    VisitList(node.ElseIfClauses),
+                    (ElseClauseSyntax?) Visit(node.ElseClause),
+                    node.EndKeyword,
+                    node.SemicolonToken);
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        public override SyntaxNode? VisitElseIfClause(ElseIfClauseSyntax node)
+        {
+            var condition = (ExpressionSyntax?) Visit(node.Condition) ?? throw new ArgumentNullException("condition");
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return node.Update(
+                    node.ElseIfKeyword,
+                    condition,
+                    node.ThenKeyword,
+                    (StatementListSyntax?) base.Visit(node.Body) ?? throw new ArgumentNullException("body"));
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        public override SyntaxNode? VisitLocalVariableDeclarationStatement(LocalVariableDeclarationStatementSyntax node)
+        {
+            var values = VisitList(node.Values);
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return node.Update(
+                    node.LocalKeyword,
+                    VisitList(node.Names),
+                    node.EqualsToken,
+                    values,
+                    node.SemicolonToken);
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        public override SyntaxNode? VisitLocalFunctionDeclarationStatement(LocalFunctionDeclarationStatementSyntax node)
+        {
+            var name = (IdentifierNameSyntax?) Visit(node.Name) ?? throw new ArgumentNullException("name");
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return node.Update(
+                    node.LocalKeyword,
+                    node.FunctionKeyword,
+                    name,
+                    (ParameterListSyntax?) base.Visit(node.Parameters) ?? throw new ArgumentNullException("parameters"),
+                    (StatementListSyntax?) base.Visit(node.Body) ?? throw new ArgumentNullException("body"),
+                    node.EndKeyword,
+                    node.SemicolonToken);
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        public override SyntaxNode? VisitFunctionDeclarationStatement(FunctionDeclarationStatementSyntax node)
+        {
+            var name = (FunctionNameSyntax?) Visit(node.Name) ?? throw new ArgumentNullException("name");
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return node.Update(
+                    node.FunctionKeyword,
+                    name,
+                    (ParameterListSyntax?) base.Visit(node.Parameters) ?? throw new ArgumentNullException("parameters"),
+                    (StatementListSyntax?) base.Visit(node.Body) ?? throw new ArgumentNullException("body"),
+                    node.EndKeyword,
+                    node.SemicolonToken);
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        public override SyntaxNode? VisitDoStatement(DoStatementSyntax node)
+        {
+            var scope = _script.GetScope(node);
+            RoslynDebug.AssertNotNull(scope);
+            try
+            {
+                _stateStack.EnterScope(scope);
+                return base.VisitDoStatement(node);
+            }
+            finally
+            {
+                _stateStack.ExitScope(scope);
+            }
+        }
+
+        #endregion Scope Pushing
+
+        #region Renaming
+
+        public override SyntaxNode? VisitNamedParameter(NamedParameterSyntax node)
+        {
+            var newName = _renameTable.GetNewVariableName(node);
+            return newName != null && node.Name != newName
+                ? node.Update(SyntaxFactory.Identifier(newName))
+                : node;
+        }
+
+        public override SyntaxNode? VisitSimpleFunctionName(SimpleFunctionNameSyntax node)
+        {
+            var newName = _renameTable.GetNewVariableName(node);
+            return newName != null && node.Name != newName
+                ? node.Update(SyntaxFactory.Identifier(newName))
+                : node;
+        }
+
+        public override SyntaxNode? VisitIdentifierName(IdentifierNameSyntax node)
+        {
+            var newName = _renameTable.GetNewVariableName(node);
+            return newName != null && node.Name != newName
+                ? node.Update(SyntaxFactory.Identifier(newName))
+                : node;
+        }
+
+        #endregion Renaming
+    }
+}
