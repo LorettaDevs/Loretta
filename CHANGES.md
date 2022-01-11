53dec7c28816fe2106cd49e74f72d63415d7c900
GGG KILLER <gggkiller2@gmail.com>
2022-01-11 20:14:17 -0300

    Add RenameVariable.

--- CHANGES.md
+++ CHANGES.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Added `IScope.ContainedScopes` which lets you check scopes contained within another scope.
 - Added `IVariable.CanBeAccessedIn(IScope)` which lets you check if the variable can be accessed within a given scope.
 - Added `IScope.FindVariable(string)` which lets you try to find a variable in a scope or any of its parents.
+- Added `Script.RenameVariable(IVariable variable, string newName)` which lets you rename a variable in a new script instance.
 
 ### Changed
 - Renamed `IScope.Parent` to `IScope.ContainingScope` to be more consistent with `ContainedScopes`.
---
+++ src/Compilers/Lua/Portable/Script/RenameErrors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loretta.CodeAnalysis.Lua
+{
+    /// <summary>
+    /// An error found while renaming a variable.
+    /// </summary>
+    public record RenameError;
+    /// <summary>
+    /// An error that represents the provided identifier not being supported
+    /// in a provided tree.
+    /// </summary>
+    /// <param name="SyntaxTree">
+    /// The <see cref="SyntaxTree"/> the identifier name is not supported on.
+    /// </param>
+    public record IdentifierNameNotSupportedError(SyntaxTree SyntaxTree) : RenameError;
+    /// <summary>
+    /// Represents a conflict with an existing variable.
+    /// </summary>
+    /// <param name="Variable">The variable that is conflicted with.</param>
+    public record VariableConflictError(IVariable Variable) : RenameError;
+}
