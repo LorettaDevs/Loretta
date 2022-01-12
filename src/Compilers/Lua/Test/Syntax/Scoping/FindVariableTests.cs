56fdd354a29392ad7410391ac0df066363c1f858
GGG KILLER <gggkiller2@gmail.com>
2022-01-11 21:27:40 -0300

    Add FindVariable tests.

---
+++ src/Compilers/Lua/Test/Syntax/Scoping/FindVariableTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Loretta.CodeAnalysis.Lua.Syntax.UnitTests.Scoping
+{
+    public class FindVariableTests : ScriptTestsBase
+    {
+        [Theory]
+        [Trait("Category", "Script/FindVariable")]
+        [InlineData(ScopeKind.File, "glob")]
+        [InlineData(ScopeKind.Function, "a")]
+        [InlineData(ScopeKind.Block, "b")]
+        public void Script_FindVariable_ReturnsNullWhenNoVariableIsAvailable(ScopeKind scopeKind, string name)
+        {
+            SetupScript(out _, out var innerMostScope);
+            Assert.Null(innerMostScope.FindVariable(name, scopeKind));
+        }
+
+        [Theory]
+        [Trait("Category", "Script/FindVariable")]
+        [InlineData(ScopeKind.Global, "glob")]
+        [InlineData(ScopeKind.Global, "a")]
+        [InlineData(ScopeKind.File, "a")]
+        [InlineData(ScopeKind.Global, "b")]
+        [InlineData(ScopeKind.File, "b")]
+        [InlineData(ScopeKind.Function, "b")]
+        [InlineData(ScopeKind.Global, "c")]
+        [InlineData(ScopeKind.File, "c")]
+        [InlineData(ScopeKind.Function, "c")]
+        [InlineData(ScopeKind.Block, "c")]
+        public void Script_FindVariable_ReturnsVariableWhenVariableIsAvailable(ScopeKind scopeKind, string name)
+        {
+            SetupScript(out _, out var innerMostScope);
+            Assert.NotNull(innerMostScope.FindVariable(name, scopeKind));
+        }
+
+        private static void SetupScript(out Script script, out IScope innerMostScope)
+        {
+            script = ParseScript("local a = 1\r\n" +
+                                 "function f(b)\r\n" +
+                                 "    print(b)\r\n" +
+                                 "    do\r\n" +
+                                 "        local c = 3\r\n" +
+                                 "    end\r\n" +
+                                 "end",
+                                 "glob = 2");
+
+            var firstTree = script.SyntaxTrees.First();
+            var root = Assert.IsType<CompilationUnitSyntax>(firstTree.GetRoot());
+            var functionDecl = Assert.IsType<FunctionDeclarationStatementSyntax>(root.Statements.Statements[1]);
+            var doStatement = Assert.IsType<DoStatementSyntax>(functionDecl.Body.Statements[1]);
+            innerMostScope = script.GetScope(doStatement)!;
+            Assert.NotNull(innerMostScope);
+        }
+    }
+}
--- src/Compilers/Lua/Test/Syntax/Scoping/ScriptTestsBase.cs
+++ src/Compilers/Lua/Test/Syntax/Scoping/ScriptTestsBase.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using Xunit;
 
 namespace Loretta.CodeAnalysis.Lua.Syntax.UnitTests.Scoping
@@ -11,6 +14,29 @@ namespace Loretta.CodeAnalysis.Lua.Syntax.UnitTests.Scoping
             Assert.Empty(tree.GetDiagnostics());
             var script = new Script(ImmutableArray.Create(tree));
             return (tree, script);
+        }
+
+        protected static Script ParseScript(params string[] codes)
+        {
+            var idx = 0;
+            var trees = Array.ConvertAll(codes, (code) => LuaSyntaxTree.ParseText(code, path: $"codes_{idx++}"));
+            if (trees.Any(t => t.GetDiagnostics().Any()))
+            {
+                foreach (var tree in trees)
+                {
+                    if (tree.GetDiagnostics().Any())
+                    {
+                        Console.WriteLine(tree.FilePath + ":");
+                        foreach (var diagnostic in tree.GetDiagnostics())
+                        {
+                            Console.WriteLine(diagnostic);
+                        }
+                    }
+                }
+            }
+            Assert.Empty(trees.SelectMany(tree => tree.GetDiagnostics()));
+            var script = new Script(ImmutableArray.CreateRange(trees));
+            return script;
         }
     }
 }
