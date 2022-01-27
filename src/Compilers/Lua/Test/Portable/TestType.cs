96bcad3f01b8cf3b5c5fcc366bc9fcf41975e306
GGG KILLER <gggkiller2@gmail.com>
2022-01-27 13:43:40 -0300

    Add test type constants.

---
+++ src/Compilers/Lua/Test/Portable/TestType.cs
@@ -0,0 +1,8 @@
+﻿namespace Loretta.CodeAnalysis.Lua.UnitTests
+{
+    internal static class TestType
+    {
+        public const string Unit = nameof(Unit);
+        public const string Regression = nameof(Regression);
+    }
+}
