1eb0c6da8bf54585c30d8cad567f492473046b7c
GGG KILLER <gggkiller2@gmail.com>
2022-02-13 18:41:48 -0300

    Add the VariableAttributeSyntax and LocalDeclarationNameSyntax nodes.

---
+++ src/Compilers/Lua/Portable/Syntax/LocalDeclarationNameSyntax.cs
@@ -0,0 +1,15 @@
+﻿namespace Loretta.CodeAnalysis.Lua.Syntax
+{
+    public sealed partial class LocalDeclarationNameSyntax
+    {
+        /// <summary>
+        /// The variable name.
+        /// </summary>
+        public string Name => IdentifierName.Name;
+
+        /// <summary>
+        /// The name of the attribute.
+        /// </summary>
+        public string? AttributeName => Attribute?.Name;
+    }
+}
