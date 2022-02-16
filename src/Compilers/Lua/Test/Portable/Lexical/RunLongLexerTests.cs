d9ecc64659a1d6d50a8ef24923bed6b641344d54
GGG KILLER <gggkiller2@gmail.com>
2022-02-16 16:44:32 -0300

    Don't run the long lexer tests on .NET Framework.

--- src/Compilers/Lua/Test/Portable/Lexical/RunLongLexerTests.cs
+++ src/Compilers/Lua/Test/Portable/Lexical/RunLongLexerTests.cs
@@ -1,11 +1,12 @@
-﻿using Loretta.Test.Utilities;
+﻿using Loretta.CodeAnalysis.Test.Utilities;
+using Loretta.Test.Utilities;
 
 namespace Loretta.CodeAnalysis.Lua.UnitTests.Lexical
 {
     public class RunLongLexerTests : ExecutionCondition
     {
         public override bool ShouldSkip =>
-            Environment.GetEnvironmentVariable("LORETTA_LONG_LEXER_TESTS") is not ("1" or "true" or "TRUE" or "True");
+            RuntimeUtilities.IsDesktopRuntime || Environment.GetEnvironmentVariable("LORETTA_LONG_LEXER_TESTS") is not ("1" or "true" or "TRUE" or "True");
 
         public override string SkipReason =>
             "LORETTA_LONG_LEXER_TESTS environment variable was not found.";
