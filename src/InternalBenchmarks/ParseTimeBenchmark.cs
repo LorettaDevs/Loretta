5f4783b77e2a8c1d3b6296237a4a05e812879bd2
GGG KILLER <gggkiller2@gmail.com>
2022-02-16 20:07:55 -0300

    Use the throughput columns.

--- src/InternalBenchmarks/ParseTimeBenchmark.cs
+++ src/InternalBenchmarks/ParseTimeBenchmark.cs
@@ -2,31 +2,33 @@
 using BenchmarkDotNet.Jobs;
 using Loretta.CodeAnalysis;
 using Loretta.CodeAnalysis.Lua;
-using Loretta.CodeAnalysis.Text;
+using Loretta.InternalBenchmarks.Attributes;
 
 #nullable disable
 
 namespace Loretta.InternalBenchmarks
 {
     [SimpleJob(RuntimeMoniker.Net60)]
+    [MeanThroughputColumn("File"), MedianColumn, MedianThroughputColumn("File")]
     [MemoryDiagnoser]
     public class ParseTimeBenchmark
     {
         private static readonly LuaParseOptions _parseOptions = new(LuaSyntaxOptions.All);
-        private SourceText _fileContents;
 
-        [Params("anim.lua", "rustic.lua")]
-        public string FileName { get; set; }
+        [ParamsSource(nameof(Files))]
+        public TestFile File { get; set; }
 
-        [GlobalSetup]
-        public void Setup()
+        public static IEnumerable<TestFile> Files
         {
-            using var stream = File.OpenRead(Path.Combine("samples", "benchies", FileName));
-            _fileContents = SourceText.From(stream);
+            get
+            {
+                yield return TestFile.Load("samples/benchies/anim.lua");
+                yield return TestFile.Load("samples/benchies/rustic.lua");
+            }
         }
 
         [Benchmark]
         public SyntaxTree Parse() =>
-            LuaSyntaxTree.ParseText(_fileContents, _parseOptions, FileName);
+            LuaSyntaxTree.ParseText(File.Text, _parseOptions, File.Name);
     }
 }
