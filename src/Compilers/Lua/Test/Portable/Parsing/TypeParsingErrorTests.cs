bf4891136c2458cc2fc10b699287bb3057bf3176
GGG KILLER <gggkiller2@gmail.com>
2022-03-02 20:07:44 -0300

    Add tests for when accept typed lua is false.

--- src/Compilers/Lua/Test/Portable/Parsing/TypeParsingErrorTests.cs
+++ src/Compilers/Lua/Test/Portable/Parsing/TypeParsingErrorTests.cs
@@ -322,5 +322,75 @@ namespace Loretta.CodeAnalysis.Lua.UnitTests.Parsing
             }
             EOF();
         }
+
+        [Fact]
+        public void Parser_ErrorsWhenAcceptTypedLuaIsFalse_AndTypedLuaStructuresAreFound()
+        {
+            var options = LuaSyntaxOptions.All.With(acceptTypedLua: false);
+            ParseAndValidate(
+                """
+                type T = T
+                export type T = T
+                local x: T = 1 :: T
+                local x = function<T>(p: T, ...: T): T end
+                local function x<T>(p: T, ...: T): T end
+                function x<T>(p: T, ...: T): T end
+                """,
+                options,
+                // (1,1): error LUA1016: Typed lua is not supported in this lua version
+                // type T = T
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, "type T = T").WithLocation(1, 1),
+                // (2,1): error LUA1016: Typed lua is not supported in this lua version
+                // export type T = T
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, "export type T = T").WithLocation(2, 1),
+                // (3,8): error LUA1016: Typed lua is not supported in this lua version
+                // local x: T = 1 :: T
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, ": T").WithLocation(3, 8),
+                // (3,14): error LUA1016: Typed lua is not supported in this lua version
+                // local x: T = 1 :: T
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, "1 :: T").WithLocation(3, 14),
+                // (4,11): error LUA1011: Invalid expression part 'function'
+                // local x = function<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_InvalidExpressionPart, "function").WithArguments("function").WithLocation(4, 11),
+                // (4,19): error LUA1001: Identifier expected
+                // local x = function<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_IdentifierExpected, "<").WithLocation(4, 19),
+                // (4,19): error LUA1016: Typed lua is not supported in this lua version
+                // local x = function<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, "<T>").WithLocation(4, 19),
+                // (4,24): error LUA1016: Typed lua is not supported in this lua version
+                // local x = function<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, ": T").WithLocation(4, 24),
+                // (4,32): error LUA1016: Typed lua is not supported in this lua version
+                // local x = function<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, ": T").WithLocation(4, 32),
+                // (4,36): error LUA1016: Typed lua is not supported in this lua version
+                // local x = function<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, ": T").WithLocation(4, 36),
+                // (5,17): error LUA1016: Typed lua is not supported in this lua version
+                // local function x<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, "<T>").WithLocation(5, 17),
+                // (5,22): error LUA1016: Typed lua is not supported in this lua version
+                // local function x<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, ": T").WithLocation(5, 22),
+                // (5,30): error LUA1016: Typed lua is not supported in this lua version
+                // local function x<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, ": T").WithLocation(5, 30),
+                // (5,34): error LUA1016: Typed lua is not supported in this lua version
+                // local function x<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, ": T").WithLocation(5, 34),
+                // (6,11): error LUA1016: Typed lua is not supported in this lua version
+                // function x<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, "<T>").WithLocation(6, 11),
+                // (6,16): error LUA1016: Typed lua is not supported in this lua version
+                // function x<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, ": T").WithLocation(6, 16),
+                // (6,24): error LUA1016: Typed lua is not supported in this lua version
+                // function x<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, ": T").WithLocation(6, 24),
+                // (6,28): error LUA1016: Typed lua is not supported in this lua version
+                // function x<T>(p: T, ...: T): T end
+                Diagnostic(ErrorCode.ERR_TypedLuaNotSupportedInLuaVersion, ": T").WithLocation(6, 28));
+        }
     }
 }
