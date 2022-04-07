f485c263cb71f7d3b3eb32112d7aa582f034beea
GGG KILLER <gggkiller2@gmail.com>
2022-04-07 12:17:39 -0300

    Add the regression test.

---
+++ src/Compilers/Lua/Test/Portable/Lexical/LexicalTestsBase.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace Loretta.CodeAnalysis.Lua.UnitTests.Lexical
+{
+    public abstract class LexicalTestsBase
+    {
+        protected static IEnumerable<SyntaxToken> Lex(string text, LuaSyntaxOptions? options = null) =>
+            SyntaxFactory.ParseTokens(text, options: new LuaParseOptions(options ?? LuaSyntaxOptions.All));
+
+        protected static SyntaxToken LexToken(string text, LuaSyntaxOptions? options = null)
+        {
+            var result = default(SyntaxToken);
+            foreach (var token in Lex(text, options))
+            {
+                if (result.Kind() == SyntaxKind.None)
+                {
+                    result = token;
+                }
+                else if (token.Kind() == SyntaxKind.EndOfFileToken)
+                {
+                    continue;
+                }
+                else
+                {
+                    Assert.True(false, "More than one token was lexed: " + token);
+                }
+            }
+            if (result.Kind() == SyntaxKind.None)
+            {
+                Assert.True(false, "No tokens were lexed");
+            }
+            return result;
+        }
+    }
+}
