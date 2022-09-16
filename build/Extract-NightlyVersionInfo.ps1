7b2406e61b714016aeb0557f24e71c9e90ef446f
GGG <gggkiller2@gmail.com>
2022-09-15 21:07:37 -0300

    Add the script to extract the nightly info.
    It also validates a few cases where we might not want or be able to do a nightly release.

---
+++ build/Extract-NightlyVersionInfo.ps1
@@ -0,0 +1,42 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+# Get the latest stable release flag
+$latestStableVersion = $(& git tag --sort=taggerdate | Where-Object { $_ -match '^v\d+.\d+.\d+$' } | Select-Object -Last 1)
+$commitsSinceLastRelease = $(& git log --oneline "$latestStableVersion.." | Where-Object { -not $_.Contains("[nightly ignore]") } | Measure-Object -Line).Lines
+
+# Fetch the unreleased version
+$unreleasedVersion = if ((Get-Content ./build/LibraryProject.props -Raw -ErrorAction SilentlyContinue) -cmatch '<Version>(\d+\.\d+\.\d+)(-[\w.+]+)?</Version>') {
+    "v" + $Matches[1]
+}
+else {
+    $False
+}
+
+if (-not $unreleasedVersion) {
+    Write-Host "::error title=Invalid Version::Unable to read version from LibraryProject.props"
+    exit 1
+}
+elseif ($latestStableVersion -eq $unreleasedVersion) {
+    Write-Host "::error title=Invalid Version::Version in LibraryProject.props is the same as the last stable release"
+    exit 1
+}
+
+# Get the latest nightly for our unreleased version
+$latestNightlyVersion = $(& git tag --sort=taggerdate | Where-Object { $_.StartsWith("$unreleasedVersion-nightly.") } | Select-Object -Last 1)
+$nightlyVersionToPublish = "$unreleasedVersion-nightly.$commitsSinceLastRelease"
+
+if ($latestNightlyVersion -and $nightlyVersionToPublish -eq $latestNightlyVersion) {
+    Write-Host "::notice title=No Changes::This nightly build is being skipped due to no changes having been made since the last nightly"
+    exit 1
+}
+
+# Set script outputs
+Write-Output "Latest Stable: $latestStableVersion"
+Write-Output "Commits Since Last Stable: $commitsSinceLastRelease"
+Write-Output "Next Stable Release: $unreleasedVersion"
+Write-Output "Latest Nightly Release: $latestNightlyVersion"
+Write-Output "Next Nightly Version: $nightlyVersionToPublish (no v: $($nightlyVersionToPublish.Substring(1)))"
+
+Write-Output "::set-output name=VERSION::$nightlyVersionToPublish"
+Write-Output "::set-output name=NO_PREFIX_VERSION::$($nightlyVersionToPublish.Substring(1))"
