9e614ccfdf94e0819de3bb3b5314eea806b7fd0f
GGG <gggkiller2@gmail.com>
2023-04-26 08:21:25 +0000

    Fix dev container setup script.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
-#! /usr/env bash
-set -eu
+#! /usr/bin/env bash
+set -euo pipefail
 
 # Unset NixOS specific configs
-git config -f /home/vscode/.config/git/config --unset core.editor || {}
-git config -f /home/vscode/.config/git/config --unset core.pager || {}
-git config -f /home/vscode/.config/git/config --unset credential.credentialstore || {}
-git config -f /home/vscode/.config/git/config --unset credential.helper || {}
-git config -f /home/vscode/.config/git/config --unset 'credential.https://github.com.helper' || {}
-git config -f /home/vscode/.config/git/config --unset interactive.difffilter || {}
+git config -f /home/vscode/.config/git/config --unset core.editor || true
+git config -f /home/vscode/.config/git/config --unset core.pager || true
+git config -f /home/vscode/.config/git/config --unset credential.credentialstore || true
+git config -f /home/vscode/.config/git/config --unset credential.helper || true
+git config -f /home/vscode/.config/git/config --unset 'credential.https://github.com.helper' || true
+git config -f /home/vscode/.config/git/config --unset interactive.difffilter || true
 
 # Restore the project packages
 dotnet restore
