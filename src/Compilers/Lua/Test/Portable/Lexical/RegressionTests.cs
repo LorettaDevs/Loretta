5db346ea6e7a98e39636eac7f3dd00526546bbb1
GGG <gggkiller2@gmail.com>
2024-03-10 11:44:56 -0300

    Add regression test for #120.
    Fixes #120.

--- src/Compilers/Lua/Test/Portable/Lexical/RegressionTests.cs
+++ src/Compilers/Lua/Test/Portable/Lexical/RegressionTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using Loretta.Test.Utilities;
+using Xunit;
 
 namespace Loretta.CodeAnalysis.Lua.UnitTests.Lexical
 {
@@ -23,6 +24,22 @@ namespace Loretta.CodeAnalysis.Lua.UnitTests.Lexical
             Assert.Equal(SyntaxKind.StringLiteralToken, token.Kind());
             Assert.Equal(RawText, token.Text);
             Assert.Equal(Value, token.Value);
+        }
+
+        [Fact]
+        [WorkItem(120, "https://github.com/LorettaDevs/Loretta/issues/120")]
+        [Trait("Type", TestType.Regression)]
+        [Trait("Category", "Lexer/Diagnostics")]
+        public void Lexer_Lexes_HexIntegersProperlyWhenPresetDoesntSupportIntegers()
+        {
+            const string RawText = "0X049bbe662";
+
+            var token = LexToken(RawText, LuaSyntaxOptions.Lua51);
+
+            Assert.Equal(SyntaxKind.NumericLiteralToken, token.Kind());
+            Assert.Equal(RawText, token.Text);
+            Assert.Equal((double) 0x049bbe662, token.Value);
+            Assert.False(token.ContainsDiagnostics);
         }
     }
 }
