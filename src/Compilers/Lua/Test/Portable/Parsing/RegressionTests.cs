6863c0342287ffc49445fc5438f42f4616339cce
GGG <gggkiller2@gmail.com>
2025-03-17 20:12:24 -0300

    Also add LanguageParser regression tests.

--- src/Compilers/Lua/Test/Portable/Parsing/RegressionTests.cs
+++ src/Compilers/Lua/Test/Portable/Parsing/RegressionTests.cs
@@ -152,5 +152,109 @@ namespace Loretta.CodeAnalysis.Lua.UnitTests.Parsing
                 // (1,13): error LUA0021: Bitwise operators are not supported in this lua version
                 // local x = y | z
                 Diagnostic(ErrorCode.ERR_BitwiseOperatorsNotSupportedInVersion, "|").WithLocation(1, 13));
+
+        [Fact, WorkItem(127, "https://github.com/LorettaDevs/Loretta/issues/127")]
+        public void LanguageParser_ProperlyTreatsContinueAsNormalIdentifierWhenContinueTypeIsNone()
+        {
+            var initial = ParseWithRoundTripCheck(
+                """
+                local continue = true
+
+                if continue then
+                    continue = false
+                end
+                """,
+                new LuaParseOptions(LuaSyntaxOptions.Lua51));
+
+            UsingNode((LuaSyntaxNode) initial.GetRoot());
+            N(SyntaxKind.CompilationUnit);
+            {
+                N(SyntaxKind.StatementList);
+                {
+                    N(SyntaxKind.LocalVariableDeclarationStatement);
+                    {
+                        N(SyntaxKind.LocalKeyword);
+                        N(SyntaxKind.LocalDeclarationName);
+                        {
+                            N(SyntaxKind.IdentifierName);
+                            {
+                                N(SyntaxKind.IdentifierToken, "continue");
+                            }
+                            N(SyntaxKind.EqualsValuesClause);
+                            {
+                                N(SyntaxKind.EqualsToken);
+                                N(SyntaxKind.TrueLiteralExpression);
+                                {
+                                    N(SyntaxKind.TrueKeyword);
+                                }
+                            }
+                        }
+                    }
+
+                    N(SyntaxKind.IfStatement);
+                    {
+                        N(SyntaxKind.IfKeyword);
+                        N(SyntaxKind.IdentifierName);
+                        {
+                            N(SyntaxKind.IdentifierToken, "continue");
+                        }
+                        N(SyntaxKind.ThenKeyword);
+
+                        N(SyntaxKind.StatementList);
+                        {
+                            N(SyntaxKind.AssignmentStatement);
+                            {
+                                N(SyntaxKind.IdentifierName);
+                                {
+                                    N(SyntaxKind.IdentifierToken, "continue");
+                                }
+                                
+                                N(SyntaxKind.EqualsValuesClause);
+                                {
+                                    N(SyntaxKind.EqualsToken);
+                                    N(SyntaxKind.FalseLiteralExpression);
+                                    {
+                                        N(SyntaxKind.FalseKeyword);
+                                    }
+                                }
+                            }
+                        }
+
+                        N(SyntaxKind.EndKeyword);
+                    }
+                }
+                N(SyntaxKind.EndOfFileToken);
+            }
+            EOF();
+        }
+        
+        [Fact, WorkItem(127, "https://github.com/LorettaDevs/Loretta/issues/127")]
+        public void LanguageParser_DoesNotFindGotosNorGotoLabelsWhenAcceptGotoIsNotTrue() =>
+            ParseAndValidate("::label:: goto label", LuaSyntaxOptions.Lua51,
+                // (1,1): error LUA1012: Invalid statement
+                // ::label:: goto label
+                Diagnostic(ErrorCode.ERR_InvalidStatement, ":").WithLocation(1, 1),
+                // (1,2): error LUA1012: Invalid statement
+                // ::label:: goto label
+                Diagnostic(ErrorCode.ERR_InvalidStatement, ":labe").WithLocation(1, 2),
+                // (1,9): error LUA1001: Identifier expected
+                // ::label:: goto label
+                Diagnostic(ErrorCode.ERR_IdentifierExpected, ":").WithLocation(1, 9),
+                // (1,9): error LUA1006: Syntax error, '(' expected
+                // ::label:: goto label
+                Diagnostic(ErrorCode.ERR_SyntaxError, ":").WithArguments("(", ":").WithLocation(1, 9),
+                // (1,9): error LUA1011: Invalid expression part ':'
+                // ::label:: goto label
+                Diagnostic(ErrorCode.ERR_InvalidExpressionPart, ":").WithArguments(":").WithLocation(1, 9),
+                // (1,9): error LUA1003: ) expected
+                // ::label:: goto label
+                Diagnostic(ErrorCode.ERR_CloseParenExpected, ":").WithLocation(1, 9),
+                // (1,16): error LUA1006: Syntax error, '(' expected
+                // ::label:: goto label
+                Diagnostic(ErrorCode.ERR_SyntaxError, "label").WithArguments("(", "").WithLocation(1, 16),
+                // (1,21): error LUA1003: ) expected
+                // ::label:: goto label
+                Diagnostic(ErrorCode.ERR_CloseParenExpected, "").WithLocation(1, 21));
+        
     }
 }
