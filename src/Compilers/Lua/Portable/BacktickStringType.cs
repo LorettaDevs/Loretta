2e021e3b56e8df1bb0b7eb105e042080561dfac6
GGG <gggkiller2@gmail.com>
2025-03-20 15:23:02 -0300

    Add option to enable interpolated strings.

---
+++ src/Compilers/Lua/Portable/BacktickStringType.cs
@@ -0,0 +1,15 @@
+﻿namespace Loretta.CodeAnalysis.Lua
+{
+    /// <summary>Defines what they type of strings using <c>`</c> delimiters will be parsed as.</summary>
+    public enum BacktickStringType
+    {
+        /// <summary>Strings with <c>`</c> delimiters have no meaning and will generate errors for unsupported interpolations.</summary>
+        None,
+
+        /// <summary>Strings with <c>`</c> delimiters will be parsed as FiveM hash string literals.</summary>
+        HashLiteral,
+
+        /// <summary>Strings with <c>`</c> delimiters will be parsed as interpolated string literals.</summary>
+        InterpolatedStringLiteral,
+    }
+}
